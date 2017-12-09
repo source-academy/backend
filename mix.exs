ac9f4f700edd04d12facad7c1988a2a0126efe76
Evan Sebastian <evanlhoini@gmail.com>
2017-12-09 12:00:27 +0800

    Switch to dev version of elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Cadet.Mixfile do
     [
       app: :cadet,
       version: "0.0.1",
-      elixir: "~> 1.6",
+      elixir: "1.6.0-dev",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
