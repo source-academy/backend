6887882a0d5cc3ed3399d0dc271084f70ea3a112
Evan Sebastian <evan.sebastian@grabtaxi.com>
2018-01-02 01:10:10 +0800

    Fix migration script

--- priv/repo/migrations/20180101144251_create_users.exs
+++ priv/repo/migrations/20180101144251_create_users.exs
@@ -10,6 +10,7 @@ defmodule Cadet.Repo.Migrations.CreateUsers do
       add(:first_name, :string, null: false)
       add(:last_name, :string)
       add(:role, :role, null: false)
+      timestamps()
     end
   end
 
