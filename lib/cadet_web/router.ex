6ac44ed715f473a07362a3bd9699e3c2650db973
Evan Sebastian <evan.sebastian@grabtaxi.com>
2018-01-02 00:38:52 +0800

    Remove dead code

--- lib/cadet_web/router.ex
+++ lib/cadet_web/router.ex
@@ -9,10 +9,6 @@ defmodule CadetWeb.Router do
     plug(:put_secure_browser_headers)
   end
 
-  pipeline :api do
-    plug(:accepts, ["json"])
-  end
-
   scope "/", CadetWeb do
     pipe_through(:browser)
 
