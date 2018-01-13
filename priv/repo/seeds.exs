45b8851d3c0b8f5afeaf0e8d776b2650b67c9355
Evan Sebastian <evan.sebastian@grabtaxi.com>
2018-01-13 17:00:58 +0800

    Add seeded users in development

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,34 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+import Cadet.Factory
+
+if Application.get_env(:cadet, :environment) == :dev do
+  seeded_users = [
+    %{
+      first_name: "Test",
+      last_name: "Student",
+      role: :student
+    },
+    %{
+      first_name: "Test",
+      last_name: "Staff",
+      role: :staff
+    },
+    %{
+      first_name: "Test",
+      last_name: "Admin",
+      role: :admin
+    }
+  ]
+
+  Enum.each(seeded_users, fn attr ->
+    user = insert(:user, attr)
+
+    insert(:email, %{
+      uid: attr.first_name <> attr.last_name <> "@test.com",
+      token: Pbkdf2.hash_pwd_salt("password"),
+      user: user
+    })
+  end)
+end
