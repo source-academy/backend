41dcaa6d7b08e4b39a749ef666741d921a4e5657
Evan Sebastian <evan.sebastian@grabtaxi.com>
2018-01-13 18:15:17 +0800

    [skip ci] Fix class name

--- lib/cadet_web/views/error_helpers.ex
+++ lib/cadet_web/views/error_helpers.ex
@@ -7,7 +7,7 @@ defmodule CadetWeb.ErrorHelpers do
   def error_tag(form, field) do
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       {reason, _} = error
-      content_tag(:span, to_string(field) <> " " <> reason, class: "pt-intent-danger")
+      content_tag(:span, to_string(field) <> " " <> reason, class: "sa-error")
     end)
   end
 end
