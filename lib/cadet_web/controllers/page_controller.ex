a7ed91d7afcde4b81ff155c9f638ba630256a052
ning <ningyuan.sg@gmail.com>
2018-05-09 21:29:23 +0800

    Fix 401 unauthorized for unauthenticated at /
    Previous behaviour: if user is unauthenticated, respond with 401 page;
        otherwise, render /
    
    This commit: if user is unauthenticated, redirect to session/new;
        otherwise, render /
    
    The logic is implemented in PageController. router.ex was modified to
    allow unauthorised connections to reach PageController.

--- lib/cadet_web/controllers/page_controller.ex
+++ lib/cadet_web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule CadetWeb.PageController do
   use CadetWeb, :controller
 
+  alias Cadet.Auth.Guardian
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    if Guardian.Plug.authenticated?(conn) do
+      render(conn, "index.html")
+    else
+      conn |> redirect(to: session_path(conn, :new))
+    end
   end
 end
