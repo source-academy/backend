fb4d5ec4220e1dd0a92812be2833e6e600a1cd20
ning <ningyuan.sg@gmail.com>
2018-05-12 15:17:16 +0800

    Implement redirect for :unauthorized at Plug
    ...instead of at controller. (see PR #9)
    
    Would be good to pass a 'redirected-due-to-unauthorized' flag with this
    redirection, so we can cause the session/new page to load with a 'please
    sign in again' message.

--- lib/cadet/auth/error_handler.ex
+++ lib/cadet/auth/error_handler.ex
@@ -2,13 +2,18 @@ defmodule Cadet.Auth.ErrorHandler do
   @moduledoc """
   Handles authentication errors
   """
+  use CadetWeb, :controller
   import Plug.Conn
 
   def auth_error(conn, {type, _reason}, _opts) do
-    body = to_string(type)
+    if type == :unauthenticated do
+      conn |> redirect(to: session_path(conn, :new))
+    else
+      body = to_string(type)
 
-    conn
-    |> put_resp_content_type("text/html")
-    |> send_resp(401, body)
+      conn
+      |> put_resp_content_type("text/html")
+      |> send_resp(401, body)
+    end
   end
 end
