5f945b7be4c9652d0a554f1b7c8cf50a3d18df62
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-05-19 22:52:47 +0800

    Add tests for AuthController#refresh

--- test/cadet_web/controllers/auth_controller_test.exs
+++ test/cadet_web/controllers/auth_controller_test.exs
@@ -4,6 +4,7 @@ defmodule CadetWeb.AuthControllerTest do
   import Cadet.Factory
 
   alias CadetWeb.AuthController
+  alias Cadet.Auth.Guardian
 
   test "swagger" do
     AuthController.swagger_definitions()
@@ -18,26 +19,30 @@ defmodule CadetWeb.AuthControllerTest do
 
       assert response(conn, 404)
     end
+
     test "blank email", %{conn: conn} do
-      conn = post(conn, "/v1/auth", %{
-        "login" => %{ "email" => "", "password" => "password" }
-      })
+      conn =
+        post(conn, "/v1/auth", %{
+          "login" => %{"email" => "", "password" => "password"}
+        })
 
       assert response(conn, 404)
     end
 
     test "blank password", %{conn: conn} do
-      conn = post(conn, "/v1/auth", %{
-        "login" => %{ "email" => "test@test.com", "password" => "" }
-      })
+      conn =
+        post(conn, "/v1/auth", %{
+          "login" => %{"email" => "test@test.com", "password" => ""}
+        })
 
       assert response(conn, 404)
     end
 
     test "email not found", %{conn: conn} do
-      conn = post(conn, "/v1/auth", %{
-        "login" => %{ "email" => "unknown@test.com", "password" => "password" }
-      })
+      conn =
+        post(conn, "/v1/auth", %{
+          "login" => %{"email" => "unknown@test.com", "password" => "password"}
+        })
 
       assert response(conn, 403)
     end
@@ -46,14 +51,16 @@ defmodule CadetWeb.AuthControllerTest do
       test_password = "password"
       user = insert(:user)
 
-      email = insert(:email, %{
-        token: Pbkdf2.hash_pwd_salt(test_password),
-        user: user
-      })
+      email =
+        insert(:email, %{
+          token: Pbkdf2.hash_pwd_salt(test_password),
+          user: user
+        })
 
-      conn = post(conn, "/v1/auth", %{
-        "login" => %{ "email" => email.uid, "password" => "password2" }
-      })
+      conn =
+        post(conn, "/v1/auth", %{
+          "login" => %{"email" => email.uid, "password" => "password2"}
+        })
 
       assert response(conn, 403)
     end
@@ -62,28 +69,49 @@ defmodule CadetWeb.AuthControllerTest do
       test_password = "password"
       user = insert(:user)
 
-      email = insert(:email, %{
-        token: Pbkdf2.hash_pwd_salt(test_password),
-        user: user
-      })
+      email =
+        insert(:email, %{
+          token: Pbkdf2.hash_pwd_salt(test_password),
+          user: user
+        })
 
-      conn = post(conn, "/v1/auth", %{
-        "login" => %{
-          "email" => email.uid,
-          "password" => test_password
-        }
-      })
+      conn =
+        post(conn, "/v1/auth", %{
+          "login" => %{
+            "email" => email.uid,
+            "password" => test_password
+          }
+        })
 
       assert json_response(conn, 200)
     end
-
   end
 
   describe "POST /auth/refresh" do
+    test "missing parameter", %{conn: conn} do
+      conn = post(conn, "/v1/auth/refresh", %{})
 
+      assert response(conn, 404)
+    end
+
+    test "invalid token", %{conn: conn} do
+      conn = post(conn, "/v1/auth/refresh", %{"refresh_token" => "asdasd"})
+
+      assert response(conn, 401)
+    end
+
+    test "valid token", %{conn: conn} do
+      user = insert(:user)
+
+      {:ok, refresh_token, _} =
+        Guardian.encode_and_sign(user, %{}, token_type: "refresh", ttl: {52, :weeks})
+
+      conn = post(conn, "/v1/auth/refresh", %{"refresh_token" => refresh_token})
+
+      assert %{"refresh_token" => ^refresh_token, "access_token" => _} = json_response(conn, 200)
+    end
   end
 
   describe "POST /auth/logout" do
-
   end
 end
