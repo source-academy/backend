ee685f3062c9302cea15b0de00d6205c18b4fb24
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-05-19 22:22:15 +0800

    Wrote some preliminary tests

--- test/cadet_web/controllers/auth_controller_test.exs
+++ test/cadet_web/controllers/auth_controller_test.exs
@@ -11,4 +11,39 @@ defmodule CadetWeb.AuthControllerTest do
     AuthController.swagger_path_refresh(nil)
     AuthController.swagger_path_logout(nil)
   end
+
+  describe "POST /v1/auth" do
+    test "blank email", %{conn: conn} do
+      conn = post(conn, "/v1/auth", %{
+        "login" => %{ "email" => "", "password" => "password" }
+      })
+
+      assert response(conn, 404)
+    end
+
+    test "blank password", %{conn: conn} do
+      conn = post(conn, "/v1/auth", %{
+        "login" => %{ "email" => "test@test.com", "password" => "" }
+      })
+
+      assert response(conn, 404)
+    end
+
+    test "email not found", %{conn: conn} do
+      conn = post(conn, "/v1/auth", %{
+        "login" => %{ "email" => "unknown@test.com", "password" => "password" }
+      })
+
+      assert response(conn, 403)
+    end
+
+  end
+
+  describe "POST /auth/refresh" do
+
+  end
+
+  describe "POST /auth/logout" do
+
+  end
 end
