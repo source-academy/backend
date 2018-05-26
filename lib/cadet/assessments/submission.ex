9b9975441faf69a25f8c2f70111555f48337288f
Sreyans Sipani <sreyans.sipani99@gmail.com>
2018-05-26 11:26:39 +0800

    Refactored

--- lib/cadet/assessments/submission.ex
+++ lib/cadet/assessments/submission.ex
@@ -45,15 +45,17 @@ defmodule Cadet.Assessments.Submission do
 
   def convert_date(params, field) do
     if params[field] != "" && params[field] != nil do
-      timezone = Timezone.get("Asia/Singapore", Timex.now)
-      date = params[field]
-        |> String.to_integer
+      timezone = Timezone.get("Asia/Singapore", Timex.now())
+
+      date =
+        params[field]
+        |> String.to_integer()
         |> Timex.from_unix(:millisecond)
         |> Timezone.convert(timezone)
+
       Map.put(params, field, date)
     else
       params
     end
   end
-
 end
