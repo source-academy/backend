fc8e2262c0d011077e8394e3a3cd8439d18afdc4
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-06-08 23:33:44 +0800

    Implement Assessment.QuestionType entities (#35)
    * Implement question types
    
    * Add tests
    
    * Add library to ProgrammingQuestion
    
    * Remove debug output and mix format
    
    * Fix according to code review
    
    * Bypass coveralls
    
    * Comment out assessments context to allow merging

---
+++ lib/cadet/assessments.ex
@@ -0,0 +1,31 @@
+defmodule Cadet.Assessments do
+  @moduledoc """
+  Assessments context contains domain logic for assessments management such as
+  missions, sidequests, paths, etc.
+  """
+  use Cadet, :context
+
+  alias Cadet.Assessments.QuestionTypes.MCQQuestion
+  alias Cadet.Assessments.QuestionTypes.ProgrammingQuestion
+
+  # To be uncommented when assessments context is merged
+  # def create_multiple_choice_question(json_attr) when is_binary(json_attr) do
+  #   %MCQQuestion{}
+  #   |> MCQQuestion.changeset(%{raw_mcqquestion: json_attr})
+  # end
+
+  # def create_multiple_choice_question(attr = %{}) do
+  #   %MCQQuestion{}
+  #   |> MCQQuestion.changeset(attr)
+  # end
+
+  # def create_programming_question(json_attr) when is_binary(json_attr) do
+  #   %ProgrammingQuestion{}
+  #   |> ProgrammingQuestion.changeset(%{raw_programmingquestion: json_attr})
+  # end
+
+  # def create_programming_question(attr = %{}) do
+  #   %ProgrammingQuestion{}
+  #   |> ProgrammingQuestion.changeset(attr)
+  # end
+end
---
+++ lib/cadet/assessments/question_types/programming_question.ex
@@ -0,0 +1,43 @@
+defmodule Cadet.Assessments.QuestionTypes.ProgrammingQuestion do
+  @moduledoc """
+  The ProgrammingQuestion entity represents a Programming question.
+  """
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  alias Cadet.Assessments.QuestionTypes.Library
+
+  embedded_schema do
+    field(:content, :string)
+    field(:solution_template, :string)
+    field(:solution_header, :string)
+    field(:solution, :string)
+    embeds_one(:library, Library)
+    field(:raw_programmingquestion, :string, virtual: true)
+  end
+
+  @required_fields ~w(content solution_template solution)a
+  @optional_fields ~w(solution_header raw_programmingquestion)a
+
+  def changeset(question, params \\ %{}) do
+    question
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> put_programmingquestion()
+    |> cast_embed(:library, required: true, with: &Library.changeset/2)
+    |> validate_required(@required_fields)
+  end
+
+  defp put_programmingquestion(changeset) do
+    change = get_change(changeset, :raw_programmingquestion)
+
+    if change do
+      json = Poison.decode!(change)
+
+      changeset
+      |> cast(json, @required_fields)
+    else
+      changeset
+    end
+  end
+end
