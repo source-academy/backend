f53c7dd03610acb2742d7b40a755e98a9a5d9b22
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-06-09 20:07:37 +0800

    Various fixes

--- test/cadet/assessments/mission_test.exs
+++ test/cadet/assessments/mission_test.exs
@@ -7,9 +7,18 @@ defmodule Cadet.Assessments.MissionTest do
     %{
       category: :mission,
       title: "mission",
-      open_at: Timex.now(),
-      close_at: Timex.shift(Timex.now(), days: 7),
+      open_at: Timex.now() |> Timex.to_unix() |> Integer.to_string(),
+      close_at: Timex.shift(Timex.now(), days: 7) |> Timex.to_unix() |> Integer.to_string(),
       max_xp: 100
+    }
+    %{
+      category: :mission,
+      title: "mission",
+      open_at: Timex.now() |> Timex.to_unix() |> Integer.to_string(),
+      close_at: Timex.shift(Timex.now(), days: 7) |> Timex.to_unix() |> Integer.to_string(),
+      max_xp: 100,
+      cover_picture: build_upload("test/fixtures/upload.png", "image/png"),
+      mission_pdf: build_upload("test/fixtures/upload.pdf", "application/pdf")
     }
   end
 
@@ -17,8 +26,8 @@ defmodule Cadet.Assessments.MissionTest do
     %{
       category: :mission,
       title: "mission",
-      open_at: Timex.now(),
-      close_at: Timex.shift(Timex.now(), days: 7),
+      open_at: Timex.now() |> Timex.to_unix() |> Integer.to_string(),
+      close_at: Timex.shift(Timex.now(), days: 7) |> Timex.to_unix() |> Integer.to_string(),
       max_xp: -100
     }
 
