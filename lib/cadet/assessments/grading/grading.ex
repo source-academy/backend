ed0d63bdfecfa8431d94ba6fe98e1ebd2c42bfcb
remo5000 <vigneshshankar@gmail.com>
2018-06-13 18:25:27 +0800

    Add grading schema
    I included the mix.exs and mix.lock changes from account-ivle as well.

---
+++ lib/cadet/assessments/grading/grading.ex
@@ -0,0 +1,23 @@
+defmodule Cadet.Assessments.Grading do
+  @moduledoc """
+  The Grading represents the Grading of a Submission by a Student.
+  """
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  schema "gradings" do
+    field(:weight, :integer)
+    field(:marks, :integer)
+    field(:comment, :string)
+  end
+
+  @required_fields ~w(weight marks)a
+  @optional_fields ~w(comment)
+
+  def changeset(answer, params \\ %{}) do
+    answer
+    |> cast(params, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
+  end
+end
