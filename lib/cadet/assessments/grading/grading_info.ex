6437b41b0c7492b41f0863e00560bf89019d632b
remo5000 <vigneshshankar@gmail.com>
2018-06-14 11:26:52 +0800

    Add grading_info schema
    To be an embedded schema within Grading.
    
    Used no-verify for credo (have TODOs)

---
+++ lib/cadet/assessments/grading/grading_info.ex
@@ -0,0 +1,26 @@
+defmodule Cadet.Assessments.GradingInfo do
+  @moduledoc """
+  The GradingInfo represents the Grading of a particular 
+  ProgrammingQuestion.
+  """
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  embedded_schema do
+    field(:marks, :integer)
+    field(:comment, :string)
+    # TODO add once assessments is merged
+    # belongs_to(:question, Assessments.Question)
+    timestamps()
+  end
+
+  # TODO add :question once assessments is merged
+  @required_fields ~w(marks comment)a
+
+  def changeset(grading_info, params \\ %{}) do
+    grading_info
+    |> cast(params, @required_fields)
+    |> validate_required(@required_fields)
+  end
+end
