71559dc745a2519583062df6e715066c6b410719
Sreyans <sreyans.sipani99@gmail.com>
2018-06-23 17:33:29 +0800

    Initial structure for answer types (#47)
    * Initial structure for answer types
    
    * Added tests
    
    * Restructured
    
    * Restructured
    
    * Fix direct comments (#55)

--- test/cadet/assessments/question_types/mcq_choice_test.exs
+++ test/cadet/assessments/question_types/mcq_choice_test.exs
@@ -4,15 +4,17 @@ defmodule Cadet.Assessments.QuestionTypes.MCQChoiceTest do
   alias Cadet.Assessments.QuestionTypes.MCQChoice
 
   valid_changesets MCQChoice do
-    %{content: "asd", is_correct: true}
-    %{content: "asd", hint: "asd", is_correct: true}
+    %{choice_id: 1, content: "asd", is_correct: true}
+    %{choice_id: 4, content: "asd", hint: "asd", is_correct: true}
   end
 
   invalid_changesets MCQChoice do
-    %{content: "asd"}
-    %{hint: "asd"}
-    %{is_correct: false}
-    %{content: "asd", hint: "aaa"}
+    %{choice_id: 1, content: "asd"}
+    %{choice_id: 1, hint: "asd"}
+    %{choice_id: 1, is_correct: false}
+    %{choice_id: 1, content: "asd", hint: "aaa"}
     %{content: 1, is_correct: true}
+    %{choice_id: 6, content: 1, is_correct: true}
+    %{choice_id: -1, content: 1, is_correct: true}
   end
 end
