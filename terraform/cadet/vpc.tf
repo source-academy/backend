840caa6fe8bc80334cfe5ac136fe71868a9b7fcc
ning <ningyuan.sg@gmail.com>
2018-07-02 13:30:16 +0800

    Change public subnet CIDRs

--- terraform/cadet/vpc.tf
+++ terraform/cadet/vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "cadet" {
 resource "aws_subnet" "public_a" {
   vpc_id                  = "${aws_vpc.cadet.id}"
   availability_zone       = "ap-southeast-1a"
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
   tags {
@@ -22,7 +22,7 @@ resource "aws_subnet" "public_a" {
 resource "aws_subnet" "public_b" {
   vpc_id                  = "${aws_vpc.cadet.id}"
   availability_zone       = "ap-southeast-1b"
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
 
   tags {
