63eda154571ae70a5e915e7a1a0c16774bf727bc
shaowei <tuesmiddt@gmail.com>
2018-07-05 00:51:39 +0800

    Fix migrations

---
+++ priv/repo/migrations/20180704163210_fix_bugged_migrations.exs
@@ -0,0 +1,28 @@
+defmodule Cadet.Repo.Migrations.FixBuggedMigrations do
+  use Ecto.Migration
+
+  def change do
+    drop(constraint(:answers, :answers_submission_id_fkey))
+    drop(constraint(:answers, :answers_question_id_fkey))
+
+    alter table(:answers) do
+      modify(:submission_id, references(:submissions), null: false)
+      modify(:question_id, references(:questions), null: false)
+      timestamps()
+    end
+
+    drop(constraint(:questions, :questions_assessment_id_fkey))
+
+    alter table(:questions) do
+      modify(:assessment_id, references(:assessments), null: false)
+    end
+
+    drop(constraint(:submissions, :submissions_assessment_id_fkey))
+    drop(constraint(:submissions, :submissions_student_id_fkey))
+
+    alter table(:submissions) do
+      modify(:assessment_id, references(:assessments), null: false)
+      modify(:student_id, references(:users), null: false)
+    end
+  end
+end
