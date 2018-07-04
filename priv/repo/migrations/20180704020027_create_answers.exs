8c96e8cb6d5a3f1210ffdcaa11ff3ea344bbd0c4
tuesmiddt <dev@chenshaowei.com>
2018-07-04 21:01:24 +0800

    Update answers model and add migration (#86)
    * Update answers model and add migration
    
    * Add indexes to answers table
    
    * Add empty line

---
+++ priv/repo/migrations/20180704020027_create_answers.exs
@@ -0,0 +1,23 @@
+defmodule Cadet.Repo.Migrations.CreateAnswersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:answers) do
+      add(:marks, :float, default: 0.0)
+      add(:answer, :map, null: false)
+      add(:submission_id, references(:submissions, null: false))
+      add(:question_id, references(:questions, null: false))
+    end
+
+    create(
+      unique_index(
+        :answers,
+        [:submission_id, :question_id],
+        name: :answers_submission_id_question_id_index
+      )
+    )
+
+    create(index(:answers, [:submission_id]))
+    create(index(:answers, [:question_id]))
+  end
+end
