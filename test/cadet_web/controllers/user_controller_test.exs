e223e8e4d39881be946e844081388ceb77079d0f
ning <ningyuan.sg@gmail.com>
2018-07-05 14:37:31 +0800

    Add tests for 401 no JWT

--- test/cadet_web/controllers/user_controller_test.exs
+++ test/cadet_web/controllers/user_controller_test.exs
@@ -7,4 +7,11 @@ defmodule CadetWeb.UserControllerTest do
     assert is_map(UserController.swagger_definitions())
     assert is_map(UserController.swagger_path_index(nil))
   end
+
+  describe "GET /user" do
+    test "unauthorized", %{conn: conn} do
+      conn = get(conn, "/v1/user", nil)
+      assert response(conn, 401)
+    end
+  end
 end
