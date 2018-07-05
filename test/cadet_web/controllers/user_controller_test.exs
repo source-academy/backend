0cfed50101cf02650abf77fd5e650abc06659c6b
ning <ningyuan.sg@gmail.com>
2018-07-05 14:42:54 +0800

    Add tests for 200 (with JWT)

--- test/cadet_web/controllers/user_controller_test.exs
+++ test/cadet_web/controllers/user_controller_test.exs
@@ -1,7 +1,10 @@
 defmodule CadetWeb.UserControllerTest do
   use CadetWeb.ConnCase
 
+  import Cadet.Factory
+
   alias CadetWeb.UserController
+  alias Cadet.Auth.Guardian
 
   test "swagger" do
     assert is_map(UserController.swagger_definitions())
@@ -9,6 +12,13 @@ defmodule CadetWeb.UserControllerTest do
   end
 
   describe "GET /user" do
+    test "success", %{conn: conn} do
+      user = insert(:user, %{role: :student})
+      conn = Guardian.Plug.sign_in(conn, user)
+      conn = get(conn, "/v1/user", nil)
+      assert response(conn, 200)
+    end
+
     test "unauthorized", %{conn: conn} do
       conn = get(conn, "/v1/user", nil)
       assert response(conn, 401)
