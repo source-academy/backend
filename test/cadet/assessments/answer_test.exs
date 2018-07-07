4bbb034fd4154be34692e470b7253bf04c5cb33d
shaowei <tuesmiddt@gmail.com>
2018-07-08 01:04:58 +0800

    Woop

--- test/cadet/assessments/answer_test.exs
+++ test/cadet/assessments/answer_test.exs
@@ -35,36 +35,50 @@ defmodule Cadet.Assessments.AnswerTest do
       changeset = Answer.changeset(%Answer{}, params)
       assert(changeset.valid?, Kernel.inspect(params))
     end
+
+    test "valid mcq question with id params", context do
+      %{assessment: assessment, submission: submission, mcq_question: question} = context
+
+      params = %{
+        submission_id: submission.id,
+        question_id: question.id,
+        type: question.type,
+        answer: %{choice_id: 0},
+        xp: 1
+      }
+
+      changeset = Answer.changeset(%Answer{}, params)
+      assert(changeset.valid?, Kernel.inspect(params))
+    end
+
+    test "valid programming question with model params", context do
+      %{assessment: assessment, submission: submission, programming_question: question} = context
+
+      params = %{
+        submission: submission,
+        question: question,
+        type: question.type,
+        answer: %{code: "hello world"},
+        xp: 1
+      }
+
+      changeset = Answer.changeset(%Answer{}, params)
+      assert(changeset.valid?, Kernel.inspect(params))
+    end
+
+    test "valid programming question with id params", context do
+      %{assessment: assessment, submission: submission, programming_question: question} = context
+
+      params = %{
+        submission_id: submission.id,
+        question_id: question.id,
+        type: question.type,
+        answer: %{code: "hello world"},
+        xp: 1
+      }
+
+      changeset = Answer.changeset(%Answer{}, params)
+      assert(changeset.valid?, Kernel.inspect(params))
+    end
   end
-
-  # valid_changesets Answer do
-  #   # TODO: Fix answer test
-  #   %{
-  #     marks: 2,
-  #     answer: %{}
-  #   }
-
-  #   # %{
-  #   #   marks: 1,
-  #   #   answer: %{}
-  #   # }
-
-  #   # %{
-  #   #   marks: 1,
-  #   #   answer: %{}
-  #   # }
-
-  #   # %{
-  #   #   marks: 100,
-  #   #   answer: %{},
-  #   #   raw_answer: Poison.encode!(%{answer: "This is a sample json"})
-  #   # }
-  # end
-
-  # invalid_changesets Answer do
-  #   %{
-  #     xp: -2,
-  #     answer: %{}
-  #   }
-  # end
 end
