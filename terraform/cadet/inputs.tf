418ebc7071e1e391f934646aee00f2f03c7e8a51
ningOTI <ningyuan.sg@gmail.com>
2018-07-08 13:28:28 +0800

    Add postgres RDS via terraform (#81)
    * Change public subnet CIDRs
    
    * Add subnet for postgresql RDS
    
    * Add aws_db_instance
    
    * Change ssh key
    
    * Add variables for RDS

--- terraform/cadet/inputs.tf
+++ terraform/cadet/inputs.tf
@@ -6,8 +6,12 @@ variable "ami_id" {
   description = "AMI ID to be used"
 }
 
-variable "instance_type" {
+variable "ec2_instance_type" {
   description = "EC2 Instance Type"
+}
+
+variable "rds_instance_class" {
+  description = "RDS Instance Class"
 }
 
 variable "min_instance_count" {
@@ -16,6 +20,14 @@ variable "min_instance_count" {
 
 variable "max_instance_count" {
   description = "Max. Number of Instance"
+}
+
+variable "rds_allocated_storage" {
+  description = "Allocated Storage for RDS"
+}
+
+variable "rds_password" {
+  description = "Password for RDS"
 }
 
 variable "ssh_public_key" {
