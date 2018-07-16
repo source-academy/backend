7b2c009533d2b34ade102620f72711aa3cfe6e0e
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-07-16 15:09:29 +0800

    Remove json deserialisers (#122)
    * Remove json deserialisers
    
    * Remove `raw_*` virtual fields
    
    * Remove unused aliases

--- lib/cadet/assessments/question_types/programming_question.ex
+++ lib/cadet/assessments/question_types/programming_question.ex
@@ -9,29 +9,14 @@ defmodule Cadet.Assessments.QuestionTypes.ProgrammingQuestion do
     field(:solution_template, :string)
     field(:solution_header, :string)
     field(:solution, :string)
-    field(:raw_programmingquestion, :string, virtual: true)
   end
 
   @required_fields ~w(content solution_template solution)a
-  @optional_fields ~w(solution_header raw_programmingquestion)a
+  @optional_fields ~w(solution_header)a
 
   def changeset(question, params \\ %{}) do
     question
     |> cast(params, @required_fields ++ @optional_fields)
-    |> put_programmingquestion
     |> validate_required(@required_fields)
-  end
-
-  defp put_programmingquestion(changeset) do
-    change = get_change(changeset, :raw_programmingquestion)
-
-    if change do
-      json = Poison.decode!(change)
-
-      changeset
-      |> cast(json, @required_fields)
-    else
-      changeset
-    end
   end
 end
