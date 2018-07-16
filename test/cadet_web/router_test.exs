b65977f5ef9b9aaaae82c6c235f79eda99812d4a
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-07-16 11:47:53 +0800

    Add test for swagger UI

--- test/cadet_web/router_test.exs
+++ test/cadet_web/router_test.exs
@@ -1,9 +1,12 @@
 defmodule CadetWeb.RouterTest do
-  use ExUnit.Case, async: true
+  use CadetWeb.ConnCase
 
   alias CadetWeb.Router
 
-  test "Swagger" do
+  test "Swagger", %{conn: conn} do
     Router.swagger_info()
+    conn = get(conn, "/swagger/index.html")
+    assert response(conn, 200)
   end
+
 end
