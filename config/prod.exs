e1d557ed443d03fb8ea607bdca80d2b23cebe6c6
ning <ningyuan.sg@gmail.com>
2018-07-17 14:42:32 +0800

    Rename prod.secret.exs -> prod.secrets.exs for consistency

--- config/prod.exs
+++ config/prod.exs
@@ -43,4 +43,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+import_config "prod.secrets.exs"
