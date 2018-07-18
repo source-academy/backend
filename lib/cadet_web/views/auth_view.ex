5f35dc113b909bd5c7c2734d7b127a7a7d646599
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-07-19 02:14:21 +0800

    Remove cruft

--- lib/cadet_web/views/auth_view.ex
+++ lib/cadet_web/views/auth_view.ex
@@ -4,8 +4,4 @@ defmodule CadetWeb.AuthView do
   def render("token.json", %{access_token: access_token, refresh_token: refresh_token}) do
     %{access_token: access_token, refresh_token: refresh_token}
   end
-
-  def render("access_token.json", %{access_token: access_token}) do
-    %{access_token: access_token}
-  end
 end
