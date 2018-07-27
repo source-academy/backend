921470787125012bf7ebc22409064d673deb4eff
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-07-27 22:21:15 +0800

    Update xml example

--- lib/cadet/updater/xml_parser.ex
+++ lib/cadet/updater/xml_parser.ex
@@ -12,37 +12,44 @@ defmodule Cadet.Updater.XMLParser do
   <!-- ***********************************************************************
   **** MISSION #M99
   ************************************************************************ -->
-  <TASK kind="mission" number="M99" startdate="2018-07-23T00:00+08" duedate="2018-09-20T23:59+08" title="Simple Stuff">
+  <TASK kind="mission" number="M99" startdate="2018-08-24T23:59+08" duedate="2018-09-20T23:59+08" title="Simple Stuff" story="mission-99">
+
   <READING>Textbook Sections 1.1.1 to 1.1.2</READING>
+  <WEBSUMMARY>
+  Welcome to your 99th mission!
+  </WEBSUMMARY>
   <TEXT>
   Welcome to this assessment! This is the *briefing*.
 
-  This mission consists of **two tasks**. First, find a way to add two numbers together, like so:
+  This mission consists of **four tasks**.
 
-  ```
-  sum(3, 5); // returns 8
-  ```
-
-  Then, let's test out the runes library.
-
-  ```
-  mul(3, 5); // return 15
+  1. A simple PROBLEM that uses the default DEPLOYMENT and GRADERDEPLOYMENT
+   for its GRADER programs (source 1).
+  2. A sorting PROBLEM that uses it's own DEPLOYMENT AND GRADERDEPLOYMENT.
+   Here, GLOBALs and EXTERNAL are used to pre-define lists for the students
+   as well as the grader to test the functions with.
+  3. An MCQ question.
+  4. A question which makes use of an EXTERNAL library, TWO_DIM_RUNES. The
+   functions from the library TWO_DIM_RUNES are exposed to the source
+   interpreter's global namespace according to what is specified in the
+   SYMBOL elements.
   </TEXT>
+
   <PROBLEMS>
-    <PROBLEM maxgrade="2">
+    <PROBLEM maxgrade="2" type="programming">
       <TEXT>
   Your first task is to define a function `sum` that adds two numbers together.
       </TEXT>
         <SNIPPET>
           <TEMPLATE>
-  const sum = () => 0; // your solution here
+  const sum = () => 0; // Replace with your solution
 
-  /* test your program! */
+  // test your program!
   sum(3, 5); // returns 8
           </TEMPLATE>
           <SOLUTION>
   // [Marking Scheme]
-  // You may subtract 1 from the grade if the student does not use arrow functions.
+  // You may subtract 1 from grade if the student does not use arrow functions.
           </SOLUTION>
           <GRADER>
   function neiYa7eil5() {
@@ -66,41 +73,111 @@ defmodule Cadet.Updater.XMLParser do
           </GRADER>
         </SNIPPET>
       </PROBLEM>
-      <PROBLEM>
+
+      <PROBLEM maxgrade="5" type="programming">
         <TEXT>
-  Use the function `show` to display a rune!
+  Now, sort a list by any means. You are provided a list of numbers to test
+  your sort function out with.
         </TEXT>
         <SNIPPET>
           <TEMPLATE>
-  // show the rune heart_bb
+  function sort() {
+  // your solution here
+  }
+
+  // test your program!
+  sort(numbers); // should be [1, 3, 5, 7]
+          </TEMPLATE>
+          <GRADER>
+  function neiYa7eil5() {
+  return equal(list(1, 3, 5, 7), sort(numbers)) ? 1 : 0;
+  }
+
+  neiYa7eil5();
+          </GRADER>
+          <GRADER>
+  function neiYa7eil5() {
+  return equal(
+    list(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15),
+    sort(bigNumbers)
+  ) ? 4 : 0;
+  }
+
+  neiYa7eil5();
+          </GRADER>
+        </SNIPPET>
+        <DEPLOYMENT interpreter="2">
+          <EXTERNAL name="NONE">
+            <SYMBOL>numbers</SYMBOL>
+          </EXTERNAL>
+          <GLOBAL>
+            <IDENTIFIER>numbers</IDENTIFIER>
+            <!-- The source stdlib is available -->
+            <VALUE>list(5, 1, 7, 3)</VALUE>
+          </GLOBAL>
+        </DEPLOYMENT>
+        <GRADERDEPLOYMENT interpreter="2">
+          <EXTERNAL name="NONE">
+            <SYMBOL>numbers</SYMBOL>
+            <SYMBOL>bigNumbers</SYMBOL>
+          </EXTERNAL>
+          <GLOBAL>
+            <IDENTIFIER>numbers</IDENTIFIER>
+            <!-- The source stdlib is available -->
+            <VALUE>list(5, 1, 7, 3)</VALUE>
+            <IDENTIFIER>bigNumbers</IDENTIFIER>
+            <VALUE>list(15, 14, 13, 12, 11, 10, 1, 2, 3, 4, 5, 9, 8, 7, 6)</VALUE>
+          </GLOBAL>
+        </GRADERDEPLOYMENT>
+      </PROBLEM>
+
+      <PROBLEM maxgrade="1" type="mcq">
+        <TEXT>
+  What is the air-speed velocity of an unladen swallow?
+        </TEXT>
+        <CHOICE correct="false"><TEXT>5 meters per second</TEXT></CHOICE>
+        <CHOICE correct="false"><TEXT>8 meters per second</TEXT></CHOICE>
+        <CHOICE correct="true"><TEXT>11 meters per second</TEXT></CHOICE>
+        <CHOICE correct="false"><TEXT>24 meters per second</TEXT></CHOICE>
+      </PROBLEM>
+
+      <PROBLEM maxgrade="0" type="programming">
+        <TEXT>
+  You'll use runes in your next mission. Why don't you give it a try?
+        </TEXT>
+        <SNIPPET>
+          <TEMPLATE>
+  // shows the rune heart_bb
+  show(heart_bb);
           </TEMPLATE>
         </SNIPPET>
+        <DEPLOYMENT interpreter="1">
+          <EXTERNAL name="TWO_DIM_RUNES">
+            <SYMBOL>show</SYMBOL>
+            <SYMBOL>heart_bb</SYMBOL>
+          </EXTERNAL>
+        </DEPLOYMENT>
       </PROBLEM>
+
     </PROBLEMS>
+
     <TEXT>
   ## Submission
 
-  Make sure that everything for your programs to work is on the left hand side and **not** in the REPL on the right! This is because only that program is used to assess your solution.
+  Make sure that everything for your programs to work is on the left hand side
+  and **not** in the REPL on the right! This is because only that program is
+  used to assess your solution.
 
   This TEXT will not be shown in cadet, but may be useful in the generated pdf.
     </TEXT>
-    <DEPLOYMENT interpreter="1">
-      <EXTERNAL name="TWO_DIM_RUNES">
-        <SYMBOL>show</SYMBOL>
-        <SYMBOL>heart_bb</SYMBOL>
-        <SYMBOL>easter_egg2</SYMBOL>
-      </EXTERNAL>
-      <GLOBAL>
-        <IDENTIFIER>easter_egg</IDENTIFIER>
-        <VALUE>"This can be accessed through the browser console from window.easter_egg, or just easter_egg"</VALUE>
-      </GLOBAL>
-      <GLOBAL>
-        <IDENTIFIER>easter_egg2</IDENTIFIER>
-        <VALUE>"This can be accessed through the source interpreter, since it is defined in EXTERNAL as a SYMBOL"</VALUE>
-      </GLOBAL>
-    </DEPLOYMENT>
-  </TASK>
 
+    <DEPLOYMENT interpreter="1">
+    </DEPLOYMENT>
+
+    <GRADERDEPLOYMENT interpreter="1">
+    </GRADERDEPLOYMENT>
+
+  </TASK>
   </CONTENT>
   """
 
