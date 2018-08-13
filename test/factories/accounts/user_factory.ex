896537e0b74c1e50e18c15640d67d735b9809c3e
Sreyans Sipani <sreyans.sipani99@gmail.com>
2018-08-13 11:02:50 +0530

    Fixed tests

--- test/factories/accounts/user_factory.ex
+++ test/factories/accounts/user_factory.ex
@@ -12,7 +12,8 @@ defmodule Cadet.Accounts.UserFactory do
           name: Faker.Name.En.name(),
           role: Enum.random(Role.__enum_map__()),
           # Due to unique nusnet id constraint
-          nusnet_id: sequence(:nusnet_id, &("E" <> String.pad_leading(&1, 7, 0)))
+          nusnet_id:
+            sequence(:nusnet_id, &("E" <> String.pad_leading(Integer.to_string(&1), 7, "0")))
         }
       end
 
@@ -20,7 +21,8 @@ defmodule Cadet.Accounts.UserFactory do
         %User{
           name: Faker.Name.En.name(),
           role: :student,
-          nusnet_id: sequence(:nusnet_id, &("E" <> String.pad_leading(&1, 7, 0)))
+          nusnet_id:
+            sequence(:nusnet_id, &("E" <> String.pad_leading(Integer.to_string(&1), 7, "0")))
         }
       end
     end
