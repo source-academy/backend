9033f117d520788cf2d7acac4f3fdfca66846aaa
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-10-27 13:17:48 +0800

    Update ex_machina from 2.2.0 to 2.2.1

--- test/factories/factory.ex
+++ test/factories/factory.ex
@@ -4,11 +4,6 @@ defmodule Cadet.Factory do
   """
   use ExMachina.Ecto, repo: Cadet.Repo
 
-  # TODO: Remove this dialyzer tag when ex_machina > v2.2.0 is released
-  # (git commit b17ba1c is merged)
-  # fields_for has been deprecated, only raising exception
-  @dialyzer {:no_return, fields_for: 1}
-
   use Cadet.Accounts.{AuthorizationFactory, UserFactory}
 
   use Cadet.Assessments.{
