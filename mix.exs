3278ad73a72bd199fc42ab9332531b323f5c465c
jiachen247 <jiachen247@gmail.com>
2019-04-22 13:14:02 +0800

    bump floki and git_hooks

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule Cadet.Mixfile do
       {:ex_aws_lambda, "~> 2.0"},
       {:ex_json_schema, "~> 0.5"},
       {:ex_machina, "~> 2.3"},
-      {:floki, "~> 0.20.0"},
+      {:floki, "~> 0.21.0"},
       {:gettext, "~> 0.11"},
       {:guardian, "~> 1.0"},
       {:guardian_db, "~> 1.0"},
@@ -79,7 +79,7 @@ defmodule Cadet.Mixfile do
       {:excoveralls, "~> 0.8", only: :test},
       {:exvcr, "~> 0.10", only: :test},
       {:faker, "~> 0.10", only: [:dev, :test]},
-      {:git_hooks, "~> 0.2.0", only: [:dev, :test]},
+      {:git_hooks, "~> 0.3.0", only: [:dev, :test]},
       {:mock, "~> 0.3.0", only: :test}
     ]
   end
