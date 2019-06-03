3aa62b5278dc8199a9220bdd4fcdec50f1d68500
Shuming <geshuming2@gmail.com>
2019-06-03 13:08:43 +0800

    Change from :submissions to :assessments

--- priv/repo/migrations/20190603023734_add_notifications.exs
+++ priv/repo/migrations/20190603023734_add_notifications.exs
@@ -6,10 +6,11 @@ defmodule Cadet.Repo.Migrations.AddNotifications do
       add(:type, :string)
       add(:read, :boolean)
       add(:user_id, references(:users), null: false)
-      add(:submission_id, references(:submissions), null: false)
+      add(:assessment_id, references(:assessments), null: false)
+      add(:question_id, references(:questions), null: true)
       timestamps()
     end
 
-    create(index(:notifications, [:user_id, :submission_id]))
+    create(index(:notifications, [:user_id, :assessment_id]))
   end
 end
