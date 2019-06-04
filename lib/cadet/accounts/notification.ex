0e0d20fb386b2a984843e8b1a8a078c483a7ad29
Shuming <geshuming2@gmail.com>
2019-06-04 16:48:03 +0800

    Added submission_id and role to Notification

--- lib/cadet/accounts/notification.ex
+++ lib/cadet/accounts/notification.ex
@@ -3,30 +3,49 @@ defmodule Cadet.Accounts.Notification do
 
   alias Cadet.Repo
   alias Cadet.Accounts.NotificationType
+  alias Cadet.Accounts.Role
   alias Cadet.Accounts.User
   alias Cadet.Assessments.Question
   alias Cadet.Assessments.Assessment
 
-  schema "notification" do
+  schema "notifications" do
     field(:type, NotificationType)
     field(:read, :boolean)
+    field(:role, Role)
 
     belongs_to(:user, User)
     belongs_to(:assessment, Assessment)
+    belongs_to(:submission, Submission)
     belongs_to(:question, Question)
 
     timestamps()
   end
 
-  @required_fields ~w(type read user_id assessment_id)a
+  @required_fields ~w(type read role user_id)a
+  @optional_fields ~w(assessment_id submission_id question_id)
 
   def changeset(answer, params) do
     answer
-    |> cast(params, @required_fields ++ [:question_id])
+    |> cast(params, @required_fields ++ @optional_fields)
     |> validate_required(@required_fields)
+    |> validate_assessment_or_submission()
     |> foreign_key_constraint(:user)
     |> foreign_key_constraint(:assessment_id)
+    |> foreign_key_constraint(:submission_id)
     |> foreign_key_constraint(:question_id)
+  end
+
+  defp validate_assessment_or_submission(changeset) do
+    case get_change(changeset, :role) do
+      :staff ->
+        validate_required(changeset, [:submission_id])
+
+      :student ->
+        validate_required(changeset, [:assessment_id])
+
+      _ ->
+        add_error(changeset, :role, "Invalid role")
+    end
   end
 
   """
