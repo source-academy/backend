ce69feaa65a43f930e0b30043dc99d54a9aadc88
Shuming <geshuming2@gmail.com>
2019-06-17 18:03:42 +0800

    Added results to factory

--- test/factories/assessments/answer_factory.ex
+++ test/factories/assessments/answer_factory.ex
@@ -8,8 +8,81 @@ defmodule Cadet.Assessments.AnswerFactory do
       alias Cadet.Assessments.Answer
 
       def answer_factory do
+        valid_autograding_results = [
+          [
+            %{
+              "resultType" => "pass",
+              "score" => 1
+            },
+            %{
+              "resultType" => "pass",
+              "score" => 1
+            },
+            %{
+              "resultType" => "pass",
+              "score" => 1
+            }
+          ],
+          [
+            %{
+              "resultType" => "fail",
+              "expected" => "1",
+              "actual" => "2"
+            },
+            %{
+              "resultType" => "fail",
+              "expected" => "2",
+              "actual" => "3"
+            },
+            %{
+              "resultType" => "fail",
+              "expected" => "5",
+              "actual" => "8"
+            }
+          ],
+          [
+            %{
+              "resultType" => "error",
+              "errors" => [
+                %{
+                  "errorType" => "syntax",
+                  "line" => 1,
+                  "location" => "student",
+                  "errorLine" => "const f = i => i === 0 ? 0 : i < 3 ? 1 : f(i-1) + f(i-2)",
+                  "errorExplanation" => "Missing semicolon at the end of statement"
+                }
+              ]
+            },
+            %{
+              "resultType" => "error",
+              "errors" => [
+                %{
+                  "errorType" => "syntax",
+                  "line" => 1,
+                  "location" => "student",
+                  "errorLine" => "const f = i => i === 0 ? 0 : i < 3 ? 1 : f(i-1) + f(i-2)",
+                  "errorExplanation" => "Missing semicolon at the end of statement"
+                }
+              ]
+            },
+            %{
+              "resultType" => "error",
+              "errors" => [
+                %{
+                  "errorType" => "syntax",
+                  "line" => 1,
+                  "location" => "student",
+                  "errorLine" => "const f = i => i === 0 ? 0 : i < 3 ? 1 : f(i-1) + f(i-2)",
+                  "errorExplanation" => "Missing semicolon at the end of statement"
+                }
+              ]
+            }
+          ]
+        ]
+
         %Answer{
           answer: %{},
+          autograding_results: sequence(:map, valid_autograding_results),
           autograding_status: :none,
           comment: Faker.Lorem.Shakespeare.En.romeo_and_juliet()
         }
