2cfeae21affc1f3bf765e112debfad15bd8a545c
flxffy <aihui@comp.nus.edu.sg>
2019-06-24 00:36:47 +0800

    Script for creating all chatkit users

--- lib/mix/tasks/users/chat.ex
+++ lib/mix/tasks/users/chat.ex
@@ -1,10 +1,15 @@
-defmodule Mix.Tasks.Cadet.Test do
+defmodule Mix.Tasks.Cadet.Users.Chat do
   @moduledoc """
     Creates ChatKit accounts for all users in the database.
     User creation: https://pusher.com/docs/chatkit/reference/api-v3#create-a-user
     Status codes: https://pusher.com/docs/chatkit/reference/api-v3#response-and-error-codes
+
+    Note: this task is to be run after `import`.
+    Assumption: "admin" must already exist in the ChatKit instance.
   """
   use Mix.Task
+
+  require Logger
 
   import Cadet.Chat
   import Mix.EctoSQL
@@ -16,13 +21,12 @@ defmodule Mix.Tasks.Cadet.Test do
 
   def run(_args) do
     ensure_started(Repo, [])
+    HTTPoison.start()
 
     url = "https://us1.pusherplatform.io/services/chatkit/v4/#{@instance_id}/users"
 
     {:ok, token} = get_superuser_token()
     headers = [Authorization: "Bearer #{token}"]
-
-    HTTPoison.start()
 
     User
     |> Repo.all()
@@ -34,13 +38,11 @@ defmodule Mix.Tasks.Cadet.Test do
           :ok
 
         {:ok, %HTTPoison.Response{body: body}} ->
-          IO.puts(
-            "Error: #{Poison.decode!(body)["error_description"]} " <>
-              "(name: #{user.name}, user_id: #{user.id})"
-          )
+          Logger.error("Unable to create user (name: #{user.name}, user_id: #{user.id})")
+          Logger.error("error: #{Poison.decode!(body)["error_description"]}")
 
-        {:error, %HTTPoison.Error{reason: reason}} ->
-          IO.inspect(reason)
+        {:error, %HTTPoison.Error{reason: error}} ->
+          Logger.error("error: #{inspect(error, pretty: true)}")
       end
     end)
   end
