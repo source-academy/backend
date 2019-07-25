8aa06c383645e588ce51787dfbb2ed4d88f6cbcb
Shuming <geshuming2@gmail.com>
2019-07-25 23:19:50 +0800

    Removed newlines

--- lib/cadet/accounts/notification_type.ex
+++ lib/cadet/accounts/notification_type.ex
@@ -3,18 +3,14 @@ import EctoEnum
 defenum(Cadet.Accounts.NotificationType, :notification_type, [
   # Notifications for new assessments
   :new,
-
   # Notifications for autograded assessments
   :autograded,
-
   # Notifications for manually graded assessments
   :graded,
   # Notifications for unsubmitted submissions
   :unsubmitted,
-
   # Notifications for submitted assessments
   :submitted,
-
   # Notifications for chatkit
   :new_message
 ])
