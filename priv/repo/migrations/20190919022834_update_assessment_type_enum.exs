48d6da4d11b0e1657242579281029c205dd749fe
jiachen <jiachen247@users.noreply.github.com>
2019-09-19 10:51:50 +0800

    format code

--- priv/repo/migrations/20190919022834_update_assessment_type_enum.exs
+++ priv/repo/migrations/20190919022834_update_assessment_type_enum.exs
@@ -4,6 +4,6 @@ defmodule Cadet.Repo.Migrations.UpdateAssessmentTypeEnum do
   @disable_ddl_transaction true
 
   def change do
-    Ecto.Migration.execute "ALTER TYPE assessment_type ADD VALUE IF NOT EXISTS 'practical'"
+    Ecto.Migration.execute("ALTER TYPE assessment_type ADD VALUE IF NOT EXISTS 'practical'")
   end
 end
