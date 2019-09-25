33dcd07e4a908271cb09224790881ef60cdab873
jiachen247 <jiachen247@gmail.com>
2019-09-25 11:11:58 +0800

    increase luminus timeout

--- lib/cadet/accounts/luminus.ex
+++ lib/cadet/accounts/luminus.ex
@@ -212,8 +212,9 @@ defmodule Cadet.Accounts.Luminus do
   """
   def api_call(method, queries \\ [], token: token) do
     headers = [{"Ocp-Apim-Subscription-Key", @api_key}, {"Authorization", "Bearer #{token}"}]
+    options = [recv_timeout: 1000]
 
-    case HTTPoison.get(api_url(method, queries), headers) do
+    case HTTPoison.get(api_url(method, queries), headers, options) do
       {:ok, %{body: body, status_code: 200}} ->
         {:ok, Jason.decode!(body)}
 
