9c9702cd37f2c529fd1ee5df5ecd1722c112e5c8
Cheng20010201 <53768585+Cheng20010201@users.noreply.github.com>
2020-03-27 14:42:16 +0800

    Update user.ex

--- lib/cadet/accounts/user.ex
+++ lib/cadet/accounts/user.ex
@@ -14,6 +14,7 @@ defmodule Cadet.Accounts.User do
     field(:name, :string)
     field(:role, Role)
     field(:nusnet_id, :string)
+    field(:collectibles, :map)
     belongs_to(:group, Group)
     timestamps()
   end
