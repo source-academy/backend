517dca7ba2c00d16cfc154d240a034dc4f6a2b22
Cheng20010201 <53768585+Cheng20010201@users.noreply.github.com>
2020-03-29 19:41:21 +0800

    Update collectibles.ex

--- lib/cadet/accounts/collectibles.ex
+++ lib/cadet/accounts/collectibles.ex
@@ -1,12 +1,24 @@
 defmodule Cadet.Collectibles do
   import Ecto.Query
-  alias Cadet.Accounts.{Notifications, User}
-  def add_user_collectibles(user = %User{}, pic_nickname, pic_name) do
-    # add the collectibles to user.collectibles, and return user.collectibles
-    Map.put(user.collectibles, pic_nickname, pic_name)
-  end
-  def user_collectibles(user = %User{}) do
+  alias Cadet.Accounts.User
+  alias Ecto.Multi
+
+  def user_collectibles(user) do
     # simply return the collectibles of the user, within a single map
     user.collectibles
+  end
+
+  '''
+  @spec update_collectibles(
+          string(),
+          string(),
+          %User{}
+        ) ::
+          {:ok, nil}
+          | {:error, {:unauthorized | :bad_request | :internal_server_error, String.t()}}
+  '''
+
+  def update_grading_info(pic_nickname, pic_name, user) do
+
   end
 end
