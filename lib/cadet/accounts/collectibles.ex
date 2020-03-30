d2fc579c5cfd59b11db838252a3fa738242fc0c1
Cheng20010201 <53768585+Cheng20010201@users.noreply.github.com>
2020-03-30 20:40:03 +0800

    Update collectibles.ex

--- lib/cadet/accounts/collectibles.ex
+++ lib/cadet/accounts/collectibles.ex
@@ -2,23 +2,25 @@ defmodule Cadet.Collectibles do
   import Ecto.Query
   alias Cadet.Accounts.User
   alias Ecto.Multi
-
+  import Cadet.Repo
+  import Ecto.Repo
+  
+  # simply return the collectibles of the user, within a single map
   def user_collectibles(user) do
-    # simply return the collectibles of the user, within a single map
     user.collectibles
   end
 
-  '''
-  @spec update_collectibles(
-          string(),
-          string(),
-          %User{}
-        ) ::
-          {:ok, nil}
-          | {:error, {:unauthorized | :bad_request | :internal_server_error, String.t()}}
-  '''
+  def update_collectibles(pic_nickname, pic_name, user) do
+    changeset =
+    Ecto.Changeset.cast(user, %{collectibles: Map.put(user_collectibles(user), pic_nickname, pic_name)},[:collectibles])
+    Cadet.Repo.update!(changeset)
+  end
 
-  def update_grading_info(pic_nickname, pic_name, user) do
-
+  # should be idle since we are not going to delete students' collectibles
+  # but provide the function delete_collectibles here for future extension
+  def delete_all_collectibles(user) do
+    changeset =
+    Ecto.Changeset.cast(user, %{collectibles: %{}},[:collectibles])
+    Cadet.Repo.update!(changeset)
   end
 end
