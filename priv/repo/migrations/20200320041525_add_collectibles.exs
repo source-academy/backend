d052ce4e967ffdc86d681635fa78c571e59d3c0f
Cheng20010201 <53768585+Cheng20010201@users.noreply.github.com>
2020-04-02 20:45:00 +0800

    Delete original collectibles.

--- priv/repo/migrations/20200320041525_add_collectibles.exs
+++
@@ -1,9 +0,0 @@
-defmodule Cadet.Repo.Migrations.AddCollectibles do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      add( :collectibles, :map, default: %{})
-    end
-  end
-end
