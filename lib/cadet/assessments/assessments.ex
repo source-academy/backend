bb77f2c1abf90f255dca98e76c366b3d47cbb72d
ScrubWzz <wongzhengzhi@gmail.com>
2020-05-21 16:51:51 +0800

    Allow new open/close date of assessments to be before current time

--- lib/cadet/assessments/assessments.ex
+++ lib/cadet/assessments/assessments.ex
@@ -33,12 +33,6 @@ defmodule Cadet.Assessments do
         Timex.before?(close_at, open_at) ->
           {:error, {:bad_request, "New end date should occur after new opening date"}}
 
-        Timex.before?(close_at, Timex.now()) ->
-          {:error, {:bad_request, "New end date should occur after current time"}}
-
-        Timex.before?(open_at, Timex.now()) ->
-          {:error, {:bad_request, "New Opening date should occur after current time"}}
-
         Timex.equal?(previous_open_time, open_at) or Timex.after?(previous_open_time, Timex.now()) ->
           update_assessment(id, %{close_at: close_at, open_at: open_at})
 
