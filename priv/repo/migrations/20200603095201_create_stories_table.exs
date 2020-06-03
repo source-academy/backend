8742fda9c66420f95a7a0e415bbaacc5e8dfed72
Tiffany Chong <sciffany@gmail.com>
2020-06-03 17:59:42 +0800

    Add story table

---
+++ priv/repo/migrations/20200603095201_create_stories_table.exs
@@ -0,0 +1,18 @@
+defmodule Cadet.Repo.Migrations.CreateStoriesTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:stories) do
+      add(:filename, :string, null: false)
+      add(:open_at, :timestamp, null: false)
+      add(:close_at, :timestamp, null: false)
+      add(:is_published, :timestamp, null: false)
+      timestamps()
+    end
+
+    create(index(:stories, [:filename]))
+    create(index(:stories, [:open_at]))
+    create(index(:stories, [:close_at]))
+    create(index(:stories, [:is_published]))
+  end
+end
