1078b7703daa2f53a0e787612ab96613b0ee7603
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2020-06-25 17:32:52 +0800

    Fix Data Shape of POST request

--- lib/cadet/achievements/achievements.ex
+++ lib/cadet/achievements/achievements.ex
@@ -17,7 +17,23 @@ defmodule Cadet.Achievements do
     Cadet.Repo.delete_all(Achievement)
 
     for new_achievement <- new_achievements do
-      Cadet.Repo.insert(:achievement, new_achievement)
+      Cadet.Repo.insert(
+        %Achievement{
+          inferencer_id: new_achievement["id"], 
+          title: new_achievement["title"],
+          ability: new_achievement["ability"], 
+          exp: new_achievement["exp"],
+          is_task: new_achievement["isTask"], 
+          prerequisite_ids: new_achievement["prerequisiteIds"], 
+          goal: new_achievement["completionGoal"], 
+          progress: new_achievement["completionProgress"], 
+    
+          modal_image_url: new_achievement["modal"]["modalImageUrl"], 
+          description: new_achievement["modal"]["modalImageUrl"], 
+          goal_text: new_achievement["modal"]["goalText"], 
+          completion_text: new_achievement["modal"]["completionText"]
+        }
+      )
     end 
 
     :ok
