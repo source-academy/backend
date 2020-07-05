7ce60e1e81d4ddcc89f73ac031b06627ca2b2335
Aulud <44989315+Aulud@users.noreply.github.com>
2020-07-05 21:37:56 +0800

    Add verification of user role when modifying default sublanguage

--- lib/cadet_web/controllers/settings_controller.ex
+++ lib/cadet_web/controllers/settings_controller.ex
@@ -4,6 +4,8 @@ defmodule CadetWeb.SettingsController do
 
   alias Cadet.Settings
 
+  @set_sublanguage_roles ~w(staff admin)a
+
   def index(conn, _) do
     {:ok, sublanguage} = Settings.get_sublanguage()
 
@@ -11,8 +13,16 @@ defmodule CadetWeb.SettingsController do
   end
 
   def update(conn, %{"chapter" => chapter, "variant" => variant}) do
-    {:ok, sublanguage} = Settings.update_sublanguage(chapter, variant)
+    role = conn.assigns[:current_user].role
 
-    render(conn, "show.json", sublanguage: sublanguage)
+    if role in @set_sublanguage_roles do
+      {:ok, _} = Settings.update_sublanguage(chapter, variant)
+
+      text(conn, "OK")
+    else
+      conn
+      |> put_status(:forbidden)
+      |> text("User not allowed to set default Playground sublanguage.")
+    end
   end
 end
