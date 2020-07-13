625cadc2fb0b9eb9eafc2297edabc241f0c9a17a
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2020-07-13 11:11:40 +0800

    Update Documentation and Add Achievement Tests

--- lib/cadet_web/controllers/achievements_controller.ex
+++ lib/cadet_web/controllers/achievements_controller.ex
@@ -5,7 +5,7 @@ defmodule CadetWeb.AchievementsController do
   use PhoenixSwagger
 
   alias Cadet.Achievements
-  alias Cadet.Achievements.AchievementAbility
+  alias Cadet.Achievements.{AchievementGoal, AchievementAbility}
 
   # TODO: ???
 
@@ -82,85 +82,151 @@ defmodule CadetWeb.AchievementsController do
     response(401, "Unauthorised")
   end
 
+  swagger_path :update do
+    post("/achievements")
+
+    summary("Updates achievements with a new set")
+
+    security([%{JWT: []}])
+
+    parameters do
+      achievements(:body, :json, "Achievements to be updated", required: true)
+    end
+
+    response(200, "OK")
+    response(401, "Unauthorised")
+  end
+
+  swagger_path :edit do
+    post("/achievements/update")
+
+    summary("Edits an achievement")
+    security([%{JWT: []}])
+
+    parameters do
+      achievement(:body, Achievement, "Achievement to be updated", required: true)
+    end
+
+    response(200, "OK")
+    response(401, "Unauthorised")
+  end
+
+  swagger_path :delete do
+    PhoenixSwagger.Path.delete("/achievements/")
+
+    summary("Deletes an achievement")
+    security([%{JWT: []}])
+
+    parameters do
+      achievement(:body, Achievement, "The associated Achievement", required: true)
+    end
+
+    response(200, "OK")
+    response(401, "Unauthorised")
+  end
+
+  swagger_path :delete_goal do
+    PhoenixSwagger.Path.delete("/achievements/goals/")
+
+    summary("Deletes a goal of an achievement")
+    security([%{JWT: []}])
+
+    parameters do
+      goal(:body, AchievementGoal, "The associated goal", required: true)
+      achievement(:body, Achievement, "The associated Achievement", required: true)
+    end
+
+    response(200, "OK")
+    response(401, "Unauthorised")
+  end
+
   def swagger_definitions do
     %{
       Achievements:
         swagger_schema do
+          description("The Achievements a Student needs to gain exp")
           properties do
-
             title(
               :string,
               "title of the achievement"
             )
-
             ability(
               AchievementAbility,
               "ability"
             )
-
             background_image_url(
               :string,
               "URL of the achievement's background image"
             )
-
             inferencer_id(
               :integer,
               "id used for reference by inferencer"
             )
-
             exp(
               :integer,
               "exp awarded for the achievement"
             )
-
             open_at(
               :string,
               "open date of achievement"
             )
-
             close_at(
               :string,
               "close date of achievement"
             )
-
             is_task(
               :boolean,
               "if the achievement is a task or not"
             )
-
             prerequisite_ids(
               :array,
               "id of the prerequisites of the achievement"
             )
-
             position(
               :position, 
               "position of achievement in the list"
             )
-
             modal_image_url(
               :string,
               "url of the image for the modal"
             )
-
             description(
               :string,
               "description of the achievement"
             )
-
             goal_text(
               :string,
               "text to reach the goal of the achievement"
             )
-
             completion_text(
               :string,
               "text to show when goal is met"
             )
-
-
           end
-        end
+        end, 
+
+      AchievementGoal:
+        swagger_schema do
+          description("The Goals to fulfill a particular Achievement")
+          properties do
+            goal_id(
+              :integer,
+              "id of the goal for the particular achievement"
+            )
+            goal_text(
+              :string,
+              "text to show when goal is met"
+            )
+            goal_progress(
+              :integer,
+              "student's progress for the goal"
+            )
+            goal_target(
+              :string,
+              "target exp needed for the student to complete the goal"
+            )
+          end 
+        end,
     }
   end
 
---
+++ test/cadet/achievements/achievement_test.exs
@@ -0,0 +1,38 @@
+defmodule Cadet.Achievments.AchievementTest do
+  alias Cadet.Achievements.Achievement
+
+  use Cadet.ChangesetCase, entity: Achievement
+
+  describe "Changesets" do
+    test "valid changesets" do
+      assert_changeset(
+        %{
+          inferencer_id: 0, 
+          id: 0,
+          title: "Hello World",
+          ability: :Core, 
+          exp: 0,
+          open_at: Timex.now() |> Timex.to_unix() |> Integer.to_string(),
+          close_at: Timex.now() |> Timex.shift(days: 7) |> Timex.to_unix() |> Integer.to_string(),
+          is_task: false
+        },
+        :valid
+      )
+    end
+
+    test "invalid changesets" do
+      assert_changeset(
+        %{
+          inferencer_id: 0, 
+          id: 0,
+          title: "Hello World",
+          ability: :Core, 
+          exp: 0,
+          open_at: Timex.now() |> Timex.shift(days: 7) |> Timex.to_unix() |> Integer.to_string(),
+          close_at: Timex.now() |> Timex.to_unix() |> Integer.to_string()
+        },
+        :invalid
+      )
+    end
+  end
+end
