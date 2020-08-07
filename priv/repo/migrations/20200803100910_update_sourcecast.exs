694d248f6d76edb31561bac3c95d163de2565a54
angelsl <angelsl@in04.sg>
2020-08-08 04:07:56 +0800

    Fix 20200803100910_update_sourcecast.exs when Sourcecasts exist

--- priv/repo/migrations/20200803100910_update_sourcecast.exs
+++ priv/repo/migrations/20200803100910_update_sourcecast.exs
@@ -2,8 +2,10 @@ defmodule Cadet.Repo.Migrations.UpdateSourcecast do
   use Ecto.Migration
 
   def change do
+    execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
+
     alter table(:sourcecasts) do
-      add(:uid, :string, null: false)
+      add(:uid, :string, null: false, default: fragment("uuid_generate_v4()"))
     end
 
     create(unique_index(:sourcecasts, [:uid]))
