9363a7a23ad43c83b54bb6f5266f1b11eff6bf61
angelsl <angelsl@in04.sg>
2021-02-22 05:27:19 +0800

    Avoid snake-casifying meta field in goal

--- lib/cadet_web/admin_controllers/admin_goals_controller.ex
+++ lib/cadet_web/admin_controllers/admin_goals_controller.ex
@@ -30,9 +30,12 @@ defmodule CadetWeb.AdminGoalsController do
   end
 
   defp json_to_goal(json, uuid \\ nil) do
+    original_meta = json["meta"]
+
     json =
       json
       |> snake_casify_string_keys_recursive()
+      |> Map.put("meta", original_meta)
 
     if is_nil(uuid) do
       json
