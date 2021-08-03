be6ee1e675d071341342b21e2f32420ae15a56a4
Chen Yanyu <39845424+YaleChen299@users.noreply.github.com>
2021-08-04 05:55:24 +0800

    Namespace game assets (#792)
    * update assets with course
    
    * formatting
    
    * update assets model test
    
    * Fix tests
    
    Co-authored-by: angelsl <angelsl@in04.sg>

--- test/cadet/assets/assets_test.exs
+++ test/cadet/assets/assets_test.exs
@@ -7,24 +7,30 @@ defmodule Cadet.Assets.AssetsTest do
   describe "Manage assets" do
     test "accessible folder" do
       use_cassette "aws/model_list_assets#1" do
-        assert Assets.list_assets("testFolder") === [
-                 "testFolder/",
-                 "testFolder/test.png",
-                 "testFolder/test2.png"
+        assert Assets.list_assets(1, "testFolder") === [
+                 "1/testFolder/",
+                 "1/testFolder/test.png",
+                 "1/testFolder/test2.png"
                ]
+      end
+    end
+
+    test "access another course with 0 folder" do
+      use_cassette "aws/model_list_assets#2" do
+        assert Assets.list_assets(2, "testFolder") === []
       end
     end
 
     test "delete nonexistent file" do
       use_cassette "aws/model_delete_asset#1" do
-        assert Assets.delete_object("testFolder", "test4.png") ===
+        assert Assets.delete_object(1, "testFolder", "test4.png") ===
                  {:error, {:not_found, "File not found"}}
       end
     end
 
     test "delete ok file" do
       use_cassette "aws/model_delete_asset#2" do
-        assert Assets.delete_object("testFolder", "test.png") === :ok
+        assert Assets.delete_object(1, "testFolder", "test.png") === :ok
       end
     end
 
@@ -32,6 +38,7 @@ defmodule Cadet.Assets.AssetsTest do
       use_cassette "aws/model_upload_asset#1" do
         assert Assets.upload_to_s3(
                  build_upload("test/fixtures/upload.png"),
+                 1,
                  "testFolder",
                  "test2.png"
                ) ===
@@ -43,10 +50,11 @@ defmodule Cadet.Assets.AssetsTest do
       use_cassette "aws/model_upload_asset#2" do
         assert Assets.upload_to_s3(
                  build_upload("test/fixtures/upload.png"),
+                 1,
                  "testFolder",
                  "test1.png"
                ) ===
-                 "https://source-academy-assets.s3.amazonaws.com/testFolder/test1.png"
+                 "https://#{bucket()}.s3.amazonaws.com/1/testFolder/test1.png"
       end
     end
   end
@@ -54,4 +62,6 @@ defmodule Cadet.Assets.AssetsTest do
   defp build_upload(path, content_type \\ "image\png") do
     %Plug.Upload{path: path, filename: Path.basename(path), content_type: content_type}
   end
+
+  defp bucket, do: :cadet |> Application.fetch_env!(:uploader) |> Keyword.get(:assets_bucket)
 end
