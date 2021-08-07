a0cd0b567d1136f930c240bf942be514ae8e1649
angelsl <angelsl@in04.sg>
2021-08-07 23:29:21 +0800

    Add provider to unique index on username

--- priv/repo/migrations/20210807040646_add_provider_to_user.exs
+++ priv/repo/migrations/20210807040646_add_provider_to_user.exs
@@ -6,6 +6,9 @@ defmodule Cadet.Repo.Migrations.AddProviderToUser do
       add(:provider, :string, null: true)
     end
 
+    drop unique_index(:users, [:username], name: "users_nusnet_id_index")
+    create unique_index(:users, [:username, :provider])
+
     execute("update users set provider = split_part(username, '/', 1)")
     execute("update users set username = substring(username from char_length(provider) + 2)")
 
