21f37ef14a2af43b9d606803c8abfadab999e154
angelsl <angelsl@in04.sg>
2022-01-04 02:14:55 +0800

    Fix MIT claim extractor

--- lib/cadet/auth/providers/mit_claim_extractor.ex
+++ lib/cadet/auth/providers/mit_claim_extractor.ex
@@ -5,22 +5,22 @@ defmodule Cadet.Auth.Providers.MITClaimExtractor do
 
   @behaviour Cadet.Auth.Providers.OpenID.ClaimExtractor
 
-  def get_username(_claims, access_token), do: Map.get(get_userinfo(access_token), "email")
+  def get_username(_claims, access_token), do: get_userinfo(access_token, "email")
 
-  def get_name(_claims, access_token), do: Map.get(get_userinfo(access_token), "name")
+  def get_name(_claims, access_token), do: get_userinfo(access_token, "name")
 
   def get_token_type, do: "access_token"
 
-  defp get_userinfo(token) do
+  defp get_userinfo(token, key) do
     headers = [{"Authorization", "Bearer #{token}"}]
     options = [timeout: 10_000, recv_timeout: 10_000]
 
     case HTTPoison.get("https://oidc.mit.edu/userinfo", headers, options) do
       {:ok, %{body: body, status_code: 200}} ->
-        {:ok, Jason.decode!(body)}
+        body |> Jason.decode!() |> Map.get(key)
 
-      {:ok, %{status_code: status}} ->
-        {:error, :upstream, "Status code #{status} from MIT OIDC server"}
+      {:ok, _} ->
+        nil
     end
   end
 end
