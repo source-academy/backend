899494775e6db1a1462b20f8df7a1ef1694da7f2
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2023-03-13 12:51:58 +0800

    fix: fix guard clauses
    - move guard clauses to prevent unnecessary querying

--- lib/cadet/workers/NotificationWorker.ex
+++ lib/cadet/workers/NotificationWorker.ex
@@ -65,43 +65,47 @@ defmodule Cadet.Workers.NotificationWorker do
     ntype = Cadet.Notifications.get_notification_type_by_name!("AVENGER BACKLOG")
     notification_type_id = ntype.id
 
-    for course_id <- Cadet.Courses.get_all_course_ids() do
-      avengers_crs = Cadet.Accounts.CourseRegistrations.get_staffs(course_id)
+    if is_system_enabled(notification_type_id) do
+      for course_id <- Cadet.Courses.get_all_course_ids() do
+        if is_course_enabled(notification_type_id, course_id, nil) do
+          avengers_crs = Cadet.Accounts.CourseRegistrations.get_staffs(course_id)
 
-      for avenger_cr <- avengers_crs do
-        avenger = Cadet.Accounts.get_user(avenger_cr.user_id)
+          for avenger_cr <- avengers_crs do
+            avenger = Cadet.Accounts.get_user(avenger_cr.user_id)
 
-        ungraded_submissions =
-          Jason.decode!(
-            elem(
-              Cadet.Assessments.all_submissions_by_grader_for_index(avenger_cr, true, true),
-              1
-            )
-          )
+            ungraded_submissions =
+              Jason.decode!(
+                elem(
+                  Cadet.Assessments.all_submissions_by_grader_for_index(avenger_cr, true, true),
+                  1
+                )
+              )
 
-        cond do
-          length(ungraded_submissions) < ungraded_threshold ->
-            IO.puts("[AVENGER_BACKLOG] below threshold!")
+            if length(ungraded_submissions) < ungraded_threshold do
+              IO.puts("[AVENGER_BACKLOG] below threshold!")
+            else
+              IO.puts("[AVENGER_BACKLOG] SENDING_OUT")
 
-          !is_system_enabled(notification_type_id) ->
-            IO.puts("[AVENGER_BACKLOG] system-level disabled!")
+              email =
+                Email.avenger_backlog_email(
+                  ntype.template_file_name,
+                  avenger,
+                  ungraded_submissions
+                )
 
-          !is_course_enabled(notification_type_id, course_id, nil) ->
-            IO.puts("[AVENGER_BACKLOG] course-level disabled")
+              {status, email} = Mailer.deliver_now(email)
 
-          true ->
-            IO.puts("[AVENGER_BACKLOG] SENDING_OUT")
-
-            email =
-              Email.avenger_backlog_email(ntype.template_file_name, avenger, ungraded_submissions)
-
-            {status, email} = Mailer.deliver_now(email)
-
-            if status == :ok do
-              Notifications.create_sent_notification(avenger_cr.id, email.html_body)
+              if status == :ok do
+                Notifications.create_sent_notification(avenger_cr.id, email.html_body)
+              end
             end
+          end
+        else
+          IO.puts("[AVENGER_BACKLOG] course-level disabled")
         end
       end
+    else
+      IO.puts("[AVENGER_BACKLOG] system-level disabled!")
     end
 
     :ok
@@ -116,40 +120,42 @@ defmodule Cadet.Workers.NotificationWorker do
     notification_type =
       Cadet.Notifications.get_notification_type_by_name!("ASSESSMENT SUBMISSION")
 
-    submission = Cadet.Assessments.get_submission_by_id(submission_id)
-    course_id = submission.assessment.course_id
-    student_id = submission.student_id
-    assement_config_id = submission.assessment.config_id
-    course_reg = Repo.get(Cadet.Accounts.CourseRegistration, submission.student_id)
-    student = Cadet.Accounts.get_user(course_reg.user_id)
-    avenger = Cadet.Accounts.CourseRegistrations.get_avenger_of(student_id).user
+    if is_system_enabled(notification_type.id) do
+      submission = Cadet.Assessments.get_submission_by_id(submission_id)
+      course_id = submission.assessment.course_id
+      student_id = submission.student_id
+      assessment_config_id = submission.assessment.config_id
+      course_reg = Repo.get(Cadet.Accounts.CourseRegistration, submission.student_id)
+      student = Cadet.Accounts.get_user(course_reg.user_id)
+      avenger_cr = Cadet.Accounts.CourseRegistrations.get_avenger_of(student_id)
+      avenger = avenger_cr.user
 
-    cond do
-      !is_system_enabled(notification_type.id) ->
-        IO.puts("[ASSESSMENT_SUBMISSION] system-level disabled!")
+      cond do
+        !is_course_enabled(notification_type.id, course_id, assessment_config_id) ->
+          IO.puts("[ASSESSMENT_SUBMISSION] course-level disabled")
 
-      !is_course_enabled(notification_type.id, course_id, assement_config_id) ->
-        IO.puts("[ASSESSMENT_SUBMISSION] course-level disabled")
+        !is_user_enabled(notification_type.id, avenger_cr.id) ->
+          IO.puts("[ASSESSMENT_SUBMISSION] user-level disabled")
 
-      !is_user_enabled(notification_type.id, submission.student_id) ->
-        IO.puts("[ASSESSMENT_SUBMISSION] user-level disabled")
+        true ->
+          IO.puts("[ASSESSMENT_SUBMISSION] SENDING_OUT")
 
-      true ->
-        IO.puts("[ASSESSMENT_SUBMISSION] SENDING_OUT")
+          email =
+            Email.assessment_submission_email(
+              notification_type.template_file_name,
+              avenger,
+              student,
+              submission
+            )
 
-        email =
-          Email.assessment_submission_email(
-            notification_type.template_file_name,
-            avenger,
-            student,
-            submission
-          )
+          {status, email} = Mailer.deliver_now(email)
 
-        {status, email} = Mailer.deliver_now(email)
-
-        if status == :ok do
-          Notifications.create_sent_notification(course_reg.id, email.html_body)
-        end
+          if status == :ok do
+            Notifications.create_sent_notification(course_reg.id, email.html_body)
+          end
+      end
+    else
+      IO.puts("[ASSESSMENT_SUBMISSION] system-level disabled!")
     end
   end
 end
