e8a28d61cdb8f97d08d4d3d75c2bbc1c899fc9e6
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2023-06-14 01:20:48 -0700

    Fix linting

--- priv/repo/migrations/20230404082921_add_unique_constraint_notification_preferences.exs
+++ priv/repo/migrations/20230404082921_add_unique_constraint_notification_preferences.exs
@@ -2,6 +2,10 @@ defmodule Cadet.Repo.Migrations.AddUniqueConstraintNotificationPreferences do
   use Ecto.Migration
 
   def change do
-    create unique_index(:notification_preferences, [:notification_config_id, :course_reg_id], name: :single_preference_per_config)
+    create(
+      unique_index(:notification_preferences, [:notification_config_id, :course_reg_id],
+        name: :single_preference_per_config
+      )
+    )
   end
 end
