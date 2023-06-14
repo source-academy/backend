5cc0439dd545155e0ec40d207b2b9b5d5ec89a04
Santosh Muthukrishnan <muthukrishnansantosh@gmail.com>
2023-06-14 01:25:45 -0700

    Fix linting

--- lib/cadet_web/helpers/controller_helper.ex
+++ lib/cadet_web/helpers/controller_helper.ex
@@ -44,11 +44,14 @@ defmodule CadetWeb.ControllerHelper do
   end
 
   def changeset_error_to_string(changeset) do
-    Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
-      Enum.reduce(opts, msg, fn {key, value}, acc ->
-        String.replace(acc, "%{#{key}}", to_string(value))
+    errors =
+      Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+        Enum.reduce(opts, msg, fn {key, value}, acc ->
+          String.replace(acc, "%{#{key}}", to_string(value))
+        end)
       end)
-    end)
+
+    errors
     |> Enum.reduce("", fn {k, v}, acc ->
       joined_errors = Enum.join(v, "; ")
       "#{acc}#{k}: #{joined_errors}\n"
