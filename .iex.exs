6a641d26e67bc4705ab67137f1319bb6f6af9660
LuYiting0913 <e0774189@u.nus.edu>
2023-07-20 13:14:24 +0800

    Fix line endings

--- .iex.exs
+++ .iex.exs
@@ -3,3 +3,7 @@ alias Cadet.Repo
 alias Cadet.Accounts.User
 alias Cadet.Assessments.{Answer, Assessment, Question, Submission}
 alias Cadet.Courses.Group
+alias Cadet.Accounts.Team
+alias Cadet.Accounts.TeamMember
+
+
