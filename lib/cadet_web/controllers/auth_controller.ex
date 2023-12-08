dd4962d7d1036b42d1b3aec0de10a8c6a2a7a56b
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-12-09 06:26:33 +0700

    Fallback to connection for code
    Hacky fallback because SAML does not require a code to be sent in the
    authentication flow.

--- lib/cadet_web/controllers/auth_controller.ex
+++ lib/cadet_web/controllers/auth_controller.ex
@@ -23,7 +23,8 @@ defmodule CadetWeb.AuthController do
         }
       ) do
     # Code is optional as SAML flow is mainly handled by backend
-    code = Map.get(params, "code")
+    # FIXME: HACKY fallback for SAML (because no code is needed)
+    code = Map.get(params, "code", conn)
     client_id = Map.get(params, "client_id")
     redirect_uri = Map.get(params, "redirect_uri")
 
