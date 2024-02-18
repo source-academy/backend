b1a249cbbecd392202fcd7f7a2bd53469166c128
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2024-02-18 19:15:13 +0800

    fix: Change Notification/Email tests to use the new response

--- test/cadet/jobs/notification_worker/notification_worker_test.exs
+++ test/cadet/jobs/notification_worker/notification_worker_test.exs
@@ -18,9 +18,14 @@ defmodule Cadet.NotificationWorker.NotificationWorkerTest do
     submission = List.first(List.first(data.mcq_answers)).submission
 
     # setup for avenger backlog
-    {:ok, %{submissions: ungraded_submissions}} =
-      Cadet.Assessments.all_submissions_by_grader_for_index(avenger_cr, true, true)
+    {:ok, %{data: %{submissions: ungraded_submissions}}} =
+      Cadet.Assessments.submissions_by_grader_for_index(avenger_cr, %{
+        "group" => "true",
+        "ungradedOnly" => "true"
+      })
 
+    # {:ok, %{submissions: ungraded_submissions}} =
+    # Cadet.Assessments.all_submissions_by_grader_for_index(avenger_cr, true, true)
     Repo.update_all(NotificationType, set: [is_enabled: true])
     Repo.update_all(NotificationConfig, set: [is_enabled: true])
 
