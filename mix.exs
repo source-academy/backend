22b05f99cc1ce52c28b727fbe04af43ca5c317e4
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2024-02-19 12:40:45 +0800

    feat: Implement new seed for populating DB

--- mix.exs
+++ mix.exs
@@ -121,7 +121,7 @@ defmodule Cadet.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seed_wip.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"],
       "phx.server": ["cadet.server"],
---
+++ priv/repo/seed_wip.exs
@@ -0,0 +1,101 @@
+# Script for populating the database. You can run it as:
+# TODO Change the following line to the correct path
+#     mix run priv/repo/seeds_wip.exs
+#
+use Cadet, [:context, :display]
+import Cadet.Factory
+import Ecto.Query
+alias Cadet.Assessments.SubmissionStatus
+alias Cadet.Accounts.{
+  User,
+  CourseRegistration,
+}
+
+if Cadet.Env.env() == :dev do
+  number_of_assessments = 10
+  number_of_students = 100
+
+  # Course
+  course = insert(:course, %{course_name: "Mock Course", course_short_name: "CS0000S"})
+
+  # Admin and Group
+  admin_cr =
+    from(cr in CourseRegistration,
+      where: cr.user_id in subquery(from(u in User, where: u.name == ^"admin", select: u.id)),
+      select: cr
+    )
+    |> Repo.one()
+  if admin_cr == nil do
+    admin = insert(:user, %{name: "Test Admin", username: "admin", latest_viewed_course: course})
+    _admin_cr = insert(:course_registration, %{user: admin, course: course, role: :admin})
+  end
+
+  group = insert(:group, %{name: "MockGroup", leader: admin_cr})
+
+  # Users
+
+  students = for i <- 1..number_of_students do
+    student = insert(:user, %{latest_viewed_course: course})
+
+    student_cr =
+      insert(:course_registration, %{user: student, course: course, role: :student, group: group})
+
+    student_cr
+  end
+
+  # Assessments and Submissions
+  valid_assessment_types = ["Mission", "Path", "Quest"]
+
+  for i <- 1..number_of_assessments do
+    config =
+      insert(:assessment_config, %{
+        type: Enum.random(valid_assessment_types),
+        order: i,
+        course: course
+      })
+
+    assessment = insert(:assessment, %{is_published: true, config: config, course: course})
+
+    questions = case assessment.config.type do
+      "Mission" ->
+          insert_list(3, :programming_question, %{assessment: assessment, max_xp: 1_000})
+
+      "Path" ->
+          insert_list(3, :mcq_question, %{assessment: assessment, max_xp: 500})
+
+      "Quest" ->
+          insert_list(3, :programming_question, %{assessment: assessment, max_xp: 500})
+      end
+
+        submissions =
+          students
+          |> Enum.map(
+            &insert(:submission, %{
+              assessment: assessment,
+              student: &1,
+              status: Enum.random(SubmissionStatus.__enum_map__())
+            })
+          )
+
+        for submission <- submissions,
+            question <- questions do
+          case question.type do
+            :programming ->
+              insert(:answer, %{
+                xp: Enum.random(0..1_000),
+                question: question,
+                submission: submission,
+                answer: build(:programming_answer)
+              })
+
+            :mcq ->
+              insert(:answer, %{
+                xp: Enum.random(0..500),
+                question: question,
+                submission: submission,
+                answer: build(:mcq_answer)
+              })
+          end
+        end
+    end
+  end
