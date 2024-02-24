7d3f9b6cc9697da769450669e706f4bc4869f53b
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2024-02-25 06:54:30 +0800

    fix: Fix bug causing build_user_filter to filter incorrectly.
    student_id in Submissions refers to the Course Reg ID, not the User ID.

--- lib/cadet/assessments/assessments.ex
+++ lib/cadet/assessments/assessments.ex
@@ -1411,7 +1411,9 @@ defmodule Cadet.Assessments do
             submission.student_id in subquery(
               from(user in User,
                 where: ilike(user.name, ^"%#{value}%"),
-                select: user.id
+                inner_join: cr in CourseRegistration,
+                on: user.id == cr.user_id,
+                select: cr.id
               )
             )
         )
@@ -1423,7 +1425,9 @@ defmodule Cadet.Assessments do
             submission.student_id in subquery(
               from(user in User,
                 where: ilike(user.username, ^"%#{value}%"),
-                select: user.id
+                inner_join: cr in CourseRegistration,
+                on: user.id == cr.user_id,
+                select: cr.id
               )
             )
         )
