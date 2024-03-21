d2bfd924e9be49074143cfccce32c8b3b9e455f4
Xiang Yu <156517235+DesSnowy@users.noreply.github.com>
2024-03-22 01:13:03 +0800

    Added hasVotingFeature to assessmentConfig

--- priv/repo/migrations/20240320151717_rename_has_token_counter_to_has_voting_features.exs
+++
@@ -1,7 +0,0 @@
-defmodule Cadet.Repo.Migrations.RenameHasTokenCounterToHasVotingFeatures do
-  use Ecto.Migration
-
-  def change do
-    rename(table(:assessment_configs), :has_token_counter, to: :has_voting_features)
-  end
-end
