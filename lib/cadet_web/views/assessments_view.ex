2fdc1093ed1aedf97925dd1d5e23784ddbf3fc06
Xiang Yu <156517235+DesSnowy@users.noreply.github.com>
2024-04-05 23:24:46 +0800

    Added new way to display leaderboard

--- lib/cadet_web/views/assessments_view.ex
+++ lib/cadet_web/views/assessments_view.ex
@@ -67,6 +67,31 @@ defmodule CadetWeb.AssessmentsView do
     )
   end
 
+  def render("leaderboard.json", %{leaderboard: leaderboard}) do
+    render_many(leaderboard, CadetWeb.AdminAssessmentsView, "answer.json", as: :answer)
+  end
+
+  def render("answer.json", %{answer: answer}) do
+    IO.inspect(transform_map_for_view(
+      answer,
+      %{
+        student_name: :student_name,
+        answer: & &1.answer["code"],
+        popular_score: :popular_score,
+        relative_score: :relative_score
+      }
+    ))
+    transform_map_for_view(
+      answer,
+      %{
+        student_name: :student_name,
+        answer: & &1.answer["code"],
+        popular_score: :popular_score,
+        relative_score: :relative_score
+      }
+    )
+  end
+
   defp password_protected?(nil), do: false
 
   defp password_protected?(_), do: true
