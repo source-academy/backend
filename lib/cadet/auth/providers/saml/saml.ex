15f9700ac01e081fcb80fd8fd750f033859a8a22
En Rong <53928333+chownces@users.noreply.github.com>
2024-05-12 01:58:22 +0800

    Update SAML provider authorise

--- lib/cadet/auth/providers/saml/saml.ex
+++ lib/cadet/auth/providers/saml/saml.ex
@@ -21,21 +21,26 @@ defmodule Cadet.Auth.Providers.SAML do
     %{assertion_extractor: assertion_extractor} = config
 
     with {:assertion, assertion} when not is_nil(assertion) <-
-           {:assertion, Samly.get_attribute(conn)} do
+           {:assertion, Samly.get_active_assertion(conn)},
+         {:name, name} when not is_nil(name) <- {:name, assertion_extractor.get_name(assertion)},
+         {:username, username} when not is_nil(username) <-
+           {:username, assertion_extractor.get_username(assertion)} do
       {:ok,
        %{
-         token: Jason.encode!(%{name: assertion_extractor.get_name(assertion)}),
-         username: assertion_extractor.get_username(assertion)
+         token: Jason.encode!(%{name: name}),
+         username: username
        }}
     else
       {:assertion, nil} -> {:error, :invalid_credentials, "Missing SAML assertion!"}
+      {:name, nil} -> {:error, :invalid_credentials, "Missing name attribute!"}
+      {:username, nil} -> {:error, :invalid_credentials, "Missing username attribute!"}
     end
   end
 
   @spec get_name(any(), Provider.token()) ::
           {:ok, String.t()} | {:error, Provider.error(), String.t()}
   def get_name(_config, token) do
-    {:ok, Jason.decode!(token).name}
+    {:ok, Jason.decode!(token)["name"]}
   end
 end
 
