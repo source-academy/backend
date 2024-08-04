cc7a77ae0f2b55255667635430ba77dad43ca9f6
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-08-04 17:09:59 +0800

    Set up response view

--- lib/cadet_web/views/chat_view.ex
+++ lib/cadet_web/views/chat_view.ex
@@ -4,4 +4,8 @@ defmodule CadetWeb.ChatView do
   def render("conversation_init.json", %{conversation_id: id, last_message: last}) do
     %{conversationId: id, response: last}
   end
+
+  def render("conversation.json", %{conversation_id: id, response: response}) do
+    %{conversationId: id, response: response}
+  end
 end
