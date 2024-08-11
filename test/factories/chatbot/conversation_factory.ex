f1598d76e8f231a59a6a9b70c37f96f80be01443
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-08-11 15:29:16 +0800

    Fix factory

--- test/factories/chatbot/conversation_factory.ex
+++ test/factories/chatbot/conversation_factory.ex
@@ -9,7 +9,7 @@ defmodule Cadet.Chatbot.ConversationFactory do
 
       def conversation_factory do
         %Conversation{
-          user_id: build(:user),
+          user_id: build(:user).id,
           prepend_context: [%{role: "system", content: "You are a helpful assistant."}],
           messages: [%{role: "assistant", content: "Hello, how can I help you?"}]
         }
