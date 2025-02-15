9953b9c1446ade9f30306184e29d715e5f3ce097
Izumi Kyouka <izumikyouka001@gmail.com>
2025-02-15 22:08:56 +0800

    fixed failure to switch course even when no exam mode is enabled for any of a user's courses

--- lib/cadet_web/controllers/user_controller.ex
+++ lib/cadet_web/controllers/user_controller.ex
@@ -65,17 +65,19 @@ defmodule CadetWeb.UserController do
   def get_latest_viewed(conn, _) do
     user = conn.assigns.current_user
     exam_mode_course = CourseRegistrations.get_exam_mode_course(conn.assigns.current_user)
-
-    latest =
-      case exam_mode_course do
-        _ -> CourseRegistrations.get_user_course(user.id, exam_mode_course.course_id)
+    if exam_mode_course do
+      latest = CourseRegistrations.get_user_course(user.id, exam_mode_course.course_id)
+      get_course_reg_config(conn, latest)
+    else
+      latest =
+        case user.latest_viewed_course_id do
+          nil -> nil
+          _ -> CourseRegistrations.get_user_course(user.id, user.latest_viewed_course_id)
       end
-      case user.latest_viewed_course_id do
-        nil -> nil
-        _ -> CourseRegistrations.get_user_course(user.id, user.latest_viewed_course_id)
-      end
 
-    get_course_reg_config(conn, latest)
+      get_course_reg_config(conn, latest)
+    end
+
   end
 
   defp get_course_reg_config(conn, course_reg) when is_nil(course_reg) do
