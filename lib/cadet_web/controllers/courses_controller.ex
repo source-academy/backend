0a25fa8e994fa795bbf65da61b62ee63f11aedb2
Arul <official@arul.me>
2025-03-19 17:00:57 +0800

    feat: added llm_grading boolean field to course creation API

--- lib/cadet_web/controllers/courses_controller.ex
+++ lib/cadet_web/controllers/courses_controller.ex
@@ -56,6 +56,7 @@ defmodule CadetWeb.CoursesController do
       enable_achievements(:body, :boolean, "Enable achievements", required: true)
       enable_sourcecast(:body, :boolean, "Enable sourcecast", required: true)
       enable_stories(:body, :boolean, "Enable stories", required: true)
+      enable_llm_grading(:body, :boolean, "Enable LLM grading", required: false)
       source_chapter(:body, :number, "Default source chapter", required: true)
 
       source_variant(:body, Schema.ref(:SourceVariant), "Default source variant name",
@@ -97,6 +98,7 @@ defmodule CadetWeb.CoursesController do
             enable_achievements(:boolean, "Enable achievements", required: true)
             enable_sourcecast(:boolean, "Enable sourcecast", required: true)
             enable_stories(:boolean, "Enable stories", required: true)
+            enable_llm_grading(:boolean, "Enable LLM grading", required: false)
             source_chapter(:integer, "Source Chapter number from 1 to 4", required: true)
             source_variant(Schema.ref(:SourceVariant), "Source Variant name", required: true)
             module_help_text(:string, "Module help text", required: true)
@@ -111,6 +113,7 @@ defmodule CadetWeb.CoursesController do
             enable_achievements: true,
             enable_sourcecast: true,
             enable_stories: false,
+            enable_llm_grading: false,
             source_chapter: 1,
             source_variant: "default",
             module_help_text: "Help text",
