398bee0fa09ceec6c4e9cda2bfb621d6ed034499
Izumi Kyouka <izumikyouka001@gmail.com>
2025-04-09 23:10:09 +0800

    formatting

--- priv/repo/migrations/20250404231500_create_table_user_browser_focus_log.exs
+++ priv/repo/migrations/20250404231500_create_table_user_browser_focus_log.exs
@@ -3,10 +3,10 @@ defmodule Cadet.Repo.Migrations.CreateUserBrowserFocusLogTable do
 
   def change do
     create table(:user_browser_focus_log) do
-      add :user_id, references(:users), null: false
-      add :course_id, references(:courses), null: false
-      add :time, :naive_datetime, null: false
-      add :focus_type, :integer, null: false
+      add(:user_id, references(:users), null: false)
+      add(:course_id, references(:courses), null: false)
+      add(:time, :naive_datetime, null: false)
+      add(:focus_type, :integer, null: false)
     end
   end
 end
