f92f87c877468b9b726f5131f30e16db9a3cec77
Blerargh <terrysi237@gmail.com>
2025-05-01 18:15:43 +0800

    Removed unnecessary execute command in migration file

--- priv/repo/migrations/20250429081534_add_leaderboard_display_columns.exs
+++ priv/repo/migrations/20250429081534_add_leaderboard_display_columns.exs
@@ -8,14 +8,5 @@ defmodule Cadet.Repo.Migrations.AddLeaderboardDisplayColumns do
       add(:top_leaderboard_display, :integer, default: 100)
       add(:top_contest_leaderboard_display, :integer, default: 10)
     end
-
-    execute(fn ->
-      repo().update_all("courses", set: [
-        enable_overall_leaderboard: true,
-        enable_contest_leaderboard: true,
-        top_leaderboard_display: 100,
-        top_contest_leaderboard_display: 10
-      ])
-    end)
   end
 end
