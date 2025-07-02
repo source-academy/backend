2d918a3111431fa3d8b1aed59ed11545fb06bf33
tckeong <teo238540@gmail.com>
2025-07-02 15:43:22 +0800

    Improve ex_aws auth check

--- lib/cadet/logger/cloudwatch_logger.ex
+++ lib/cadet/logger/cloudwatch_logger.ex
@@ -164,8 +164,11 @@ defmodule Cadet.Logger.CloudWatchLogger do
   end
 
   defp check_exaws_config do
-    id = System.get_env("AWS_ACCESS_KEY_ID")
-    secret = System.get_env("AWS_SECRET_ACCESS_KEY")
+    id = Application.get_env(:ex_aws, :access_key_id) || System.get_env("AWS_ACCESS_KEY_ID")
+
+    secret =
+      Application.get_env(:ex_aws, :secret_access_key) || System.get_env("AWS_SECRET_ACCESS_KEY")
+
     region = Application.get_env(:ex_aws, :region) || System.get_env("AWS_REGION")
 
     cond do
