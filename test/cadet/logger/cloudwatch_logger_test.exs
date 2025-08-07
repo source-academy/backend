8b2be352ab56d5fc4935490c621948467e25c487
tckeong <teo238540@gmail.com>
2025-08-07 23:41:35 +0800

    Improve test cases

--- test/cadet/logger/cloudwatch_logger_test.exs
+++ test/cadet/logger/cloudwatch_logger_test.exs
@@ -3,6 +3,7 @@ defmodule Cadet.Logger.CloudWatchLoggerTest do
 
   require Logger
   import Mox
+  import ExUnit.CaptureLog
   alias Cadet.Logger.CloudWatchLogger
 
   setup :set_mox_from_context
@@ -16,17 +17,21 @@ defmodule Cadet.Logger.CloudWatchLoggerTest do
       level: :info,
       log_group: "test_log_group",
       log_stream: "test_log_stream",
-      format: "$time $metadata[$level] $message",
+      format: "$time $metadata[$level] $message\n",
       metadata: [:request_id]
     )
 
     LoggerBackends.add({CloudWatchLogger, :cloudwatch_logger})
 
-    Logger.configure_backend(:console, level: :error)
+    Logger.configure_backend(:console, level: :error, format: "$metadata[$level] $message\n")
 
     on_exit(fn ->
       LoggerBackends.remove({CloudWatchLogger, :cloudwatch_logger})
-      Logger.configure_backend(:console, level: :warning)
+
+      Logger.configure_backend(:console,
+        level: :warning,
+        format: "$time $metadata[$level] $message\n"
+      )
     end)
 
     :ok
@@ -61,11 +66,14 @@ defmodule Cadet.Logger.CloudWatchLoggerTest do
        }}
     end)
 
-    Logger.error("this is an error")
-    Logger.warning("this is a warning")
+    _ =
+      capture_log(fn ->
+        Logger.error("this is an error")
+        Logger.warning("this is a warning")
 
-    # wait for timer to flush the buffer
-    Process.sleep(5100)
+        # wait for timer to flush the buffer
+        Process.sleep(5100)
+      end)
   end
 
   test "Force flush the buffer when the buffer size is reached" do
@@ -111,9 +119,19 @@ defmodule Cadet.Logger.CloudWatchLoggerTest do
       {:error, "Failed to send log to CloudWatch"}
     end)
 
-    Logger.warning("this is a warning")
+    retry_message =
+      "[error] Failed to send log to CloudWatch. \"Failed to send log to CloudWatch\". Retrying..."
 
-    Process.sleep(6000)
+    failed_message = "[error] Failed to send log to CloudWatch. After multiple retries."
+
+    log =
+      capture_log(fn ->
+        Logger.warning("this is a warning")
+
+        Process.sleep(6000)
+      end)
+
+    assert String.contains?(log, retry_message) and String.contains?(log, failed_message)
   end
 
   defp assert_config(%{http_method: http_method, data: data, headers: headers, service: service}) do
