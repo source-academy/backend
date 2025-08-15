0b7ed8b11cc4611bec7e2d9ca189ad2dbb2d90a1
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-08-15 19:37:53 +0800

    Update log messages

--- lib/cadet/accounts/accounts.ex
+++ lib/cadet/accounts/accounts.ex
@@ -110,7 +110,7 @@ defmodule Cadet.Accounts do
     |> Repo.one()
     |> case do
       nil ->
-        Logger.warning("User is not enrolled in the specified course")
+        Logger.warning("User #{user_id} is not enrolled in course #{latest_viewed_course_id}")
         {:error, {:bad_request, "user is not in the course"}}
 
       _ ->
