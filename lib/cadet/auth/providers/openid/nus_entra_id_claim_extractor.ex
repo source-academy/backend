c8b97819f53947f283ed7b23fb29461ec4c1e272
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2026-02-19 20:26:53 +0800

    Simplify logic

--- lib/cadet/auth/providers/openid/nus_entra_id_claim_extractor.ex
+++ lib/cadet/auth/providers/openid/nus_entra_id_claim_extractor.ex
@@ -7,16 +7,12 @@ defmodule Cadet.Auth.Providers.NusEntraIdClaimExtractor do
 
   @dialyzer {:nowarn_function, get_username: 2}
   def get_username(claims, access_token) do
-    # TODO: Remove the debug statement after confirming the claims structure
-    Jason.decode!(claims) |> IO.inspect(label: "Claims in NUS Entra ID")
-    get_userinfo(access_token, "samAccountName")
+    get_userinfo(claims, "samAccountName")
   end
 
   @dialyzer {:nowarn_function, get_name: 2}
   def get_name(claims, access_token) do
-    # TODO: Remove the debug statement after confirming the claims structure
-    Jason.decode!(claims) |> IO.inspect(label: "Claims in NUS Entra ID")
-    get_userinfo(access_token, "displayName")
+    get_userinfo(claims, "displayName")
   end
 
   def get_token_type, do: "id_token"
@@ -25,36 +21,22 @@ defmodule Cadet.Auth.Providers.NusEntraIdClaimExtractor do
   @allowed_domains ~w(student alum staff)
 
   defp check_allowed_domain(claims) do
-    domain = Map.get(claims, "onPremisesExtensionAttributes")["extensionAttribute6"]
+    domain = Map.get(claims, "ExtensionAttribute6")
+    # TODO: Remove log
+    IO.inspect({:domain, domain})
     domain in @allowed_domains
   end
 
-  defp map_key_to_raw("samAccountName"), do: "onPremisesSamAccountName"
   defp map_key_to_raw(key), do: key
 
-  defp get_userinfo(token, key) do
-    Jason.decode!(token) |> IO.inspect(label: "Token in NUS Entra ID")
-    headers = [{"Authorization", "Bearer #{token}"}]
-    options = [timeout: 10_000, recv_timeout: 10_000]
-
-    url =
-      "https://graph.microsoft.com/v1.0/me?$select=onPremisesSamAccountName,onPremisesExtensionAttributes"
-
-    case HTTPoison.get(url, headers, options) do
-      {:ok, %{body: body, status_code: 200}} ->
-        data = Jason.decode!(body)
-
-        with true <- check_allowed_domain(data),
-             mapped_key <- map_key_to_raw(key),
-             value when not is_nil(value) <- Map.get(data, mapped_key) do
-          value
-        else
-          false -> nil
-          nil -> nil
-        end
-
-      {:ok, _} ->
-        nil
+  defp get_userinfo(claims, key) do
+    with true <- check_allowed_domain(data),
+          mapped_key <- map_key_to_raw(key),
+          value when not is_nil(value) <- Map.get(data, mapped_key) do
+      value
+    else
+      false -> nil
+      _ -> nil
     end
   end
 end
