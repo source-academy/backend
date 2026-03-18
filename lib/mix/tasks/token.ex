970ed881ba757f57d7ffda412068a7f4f345e90b
geraldnyeo <geraldnyeo@gmail.com>
2026-03-18 11:28:53 +0800

    formatting

--- lib/mix/tasks/token.ex
+++ lib/mix/tasks/token.ex
@@ -110,7 +110,11 @@ defmodule Mix.Tasks.Cadet.Token do
         })
 
         %User{}
-        |> User.changeset(%{name: "Test#{role_capitalized}", username: "test_#{role}", provider: "test"})
+        |> User.changeset(%{
+          name: "Test#{role_capitalized}",
+          username: "test_#{role}",
+          provider: "test"
+        })
         |> Repo.insert!()
 
         new_user
