70c50ffb6bfa93eebd53ef7ba3ae62e4156c75a0
Leong Yi Quan <yiquan.leong@gmail.com>
2026-03-27 09:42:01 +0800

    pass testcases for llmcost calculations

--- priv/repo/migrations/20260320094651_repair_llm_columns_on_assessments.exs
+++ priv/repo/migrations/20260320094651_repair_llm_columns_on_assessments.exs
@@ -3,12 +3,12 @@ defmodule Cadet.Repo.Migrations.RepairLlmColumnsOnAssessments do
 
   def up do
     alter table(:assessments) do
-      add_if_not_exists(:llm_input_cost, :decimal, precision: 10, scale: 4, default: 3.20)
-      add_if_not_exists(:llm_output_cost, :decimal, precision: 10, scale: 4, default: 12.80)
+      add_if_not_exists(:llm_input_cost, :decimal, precision: 10, scale: 6, default: 3.20)
+      add_if_not_exists(:llm_output_cost, :decimal, precision: 10, scale: 6, default: 12.80)
       add_if_not_exists(:llm_total_input_tokens, :integer, default: 0)
       add_if_not_exists(:llm_total_output_tokens, :integer, default: 0)
       add_if_not_exists(:llm_total_cached_tokens, :integer, default: 0)
-      add_if_not_exists(:llm_total_cost, :decimal, precision: 10, scale: 4, default: 0.0)
+      add_if_not_exists(:llm_total_cost, :decimal, precision: 10, scale: 6, default: 0.0)
     end
   end
 
